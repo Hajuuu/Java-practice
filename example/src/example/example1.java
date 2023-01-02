e76827e2a06ae82bf23ee9a64fee13550ee19e92
Juyeon Ha <hpt0127@naver.com>
2023-01-02 13:27:36 +0900

    VarEx1

--- example/src/example/example1.java
+++ example/src/example/example1.java
@@ -4,7 +4,17 @@ public class example1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hiiiii");
+		int year = 0;
+		int age = 14;
+		
+		System.out.println(year);
+		System.out.println(age);
+		
+		year = age + 2000;
+		age = age + 1;
+		
+		System.out.println(year);
+		System.out.println(age);
 
 	}
 
