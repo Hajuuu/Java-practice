192666451eb4f15ae9871a2d5b92b9f65aba4c64
Juyeon Ha <hpt0127@naver.com>
2023-01-05 20:58:06 +0900

    FlowEx24

--- example/src/example/example1.java
+++ example/src/example/example1.java
@@ -5,23 +5,20 @@ public class example1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("당신의 주민번호를 입력하세요.>");
+		int i = 11;
 		
-		Scanner scanner = new Scanner(System.in);
-		String regNo = scanner.nextLine();
+		System.out.println("카운트다운을 시작합니다.");
 		
-		char gender = regNo.charAt(7);
-		switch(gender) {
-			case '1': case '3':
-				System.out.println("당신은 남자입니다.");
-				break;
-			case '2': case '4':
-				System.out.println("당신은 여자입니다.");
-				break;
-			default:
-				System.out.println("유효하지 않은 주민등록번호입니다.");
+		while(i--!=0) {
+			System.out.println(i);
+			
+			for(int j = 0; j<2000000000;j++) {
+				;
+			}
+		
 				
 		}
+		System.out.println("GAME OVER");
 	}
 	
 
