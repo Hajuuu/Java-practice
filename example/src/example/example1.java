1bb38c98019395fdd9f9d1e82f9f4cf4e246c436
Juyeon Ha <hpt0127@naver.com>
2023-01-06 20:27:14 +0900

    ArrayEx6

--- example/src/example/example1.java
+++ example/src/example/example1.java
@@ -5,18 +5,21 @@ public class example1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int sum = 0;
-		float average = 0f;
+		int[] score = { 79, 88, 91, 33, 100, 55, 95};
 		
-		int[] score = {100, 88, 100, 100, 90};
+		int max = score[0];
+		int min = score[0];
 		
 		for(int i=0; i < score.length; i++) {
-			sum += score[i];
+			if(score[i] > max) {
+				max = score[i];
+			}else if(score[i] < min){
+				min = score[i];
+			}
 		}
-		average = sum / (float)score.length;
 		
-		System.out.println("총점 : "+sum);
-		System.out.println("평균 : "+average);
+		System.out.println("최대값 : "+max);
+		System.out.println("최소값 : "+min);
 	}
 	
 
