78e1639c019c11d8c6f30bd75eefd748119f8ffe
Juyeon Ha <hpt0127@naver.com>
2023-01-06 21:05:23 +0900

    ArrayEx11

--- example/src/example/example1.java
+++ example/src/example/example1.java
@@ -6,30 +6,20 @@ public class example1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] numArr = new int[10];
+		int[] counter = new int[10];
 		
 		for(int i=0; i < numArr.length; i++) {
-			System.out.print(numArr[i] = (int)(Math.random() * 10));
+			numArr[i] = (int)(Math.random() * 10);
+			System.out.print(numArr[i]);
 		}
 		System.out.println();
 		
 		for(int i=0; i < numArr.length; i++) {
-			boolean changed = false;
-			
-			for(int j=0; j < numArr.length-1-i; j++) {
-				if(numArr[j] > numArr[j+1]) {
-					int temp = numArr[j];
-					numArr[j] = numArr[j+1];
-					numArr[j+1] = temp;
-					changed = true;
-				}
-			}
-			
-			if(!changed) break;
-			
-			for(int k=0; k<numArr.length;k++) {
-				System.out.print(numArr[k]);
-			}
-			System.out.println();
+			counter[numArr[i]]++;
+		}
+		
+		for(int i=0; i < numArr.length; i++) {
+			System.out.println(i + "의 개수 :"+counter[i]);
 		}
 	}
 	
