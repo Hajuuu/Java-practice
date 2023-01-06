ee84d33c3293f2364da08cfb2264109c0fe88301
Juyeon Ha <hpt0127@naver.com>
2023-01-06 20:36:28 +0900

    ArrayEx7

--- example/src/example/example1.java
+++ example/src/example/example1.java
@@ -5,21 +5,24 @@ public class example1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] score = { 79, 88, 91, 33, 100, 55, 95};
+		int[] numArr = new int[10];
 		
-		int max = score[0];
-		int min = score[0];
+		for(int i=0; i < numArr.length; i++) {
+			numArr[i] = i;
+			System.out.print(numArr[i]);
+		}
+		System.out.println();
 		
-		for(int i=0; i < score.length; i++) {
-			if(score[i] > max) {
-				max = score[i];
-			}else if(score[i] < min){
-				min = score[i];
-			}
+		for(int i=0; i < 100; i++) {
+			int n = (int) (Math.random() * 10);
+			int tmp = numArr[0];
+			numArr[0] = numArr[n];
+			numArr[n] = tmp;
 		}
 		
-		System.out.println("최대값 : "+max);
-		System.out.println("최소값 : "+min);
+		for(int i=0; i < numArr.length; i++) {
+			System.out.print(numArr[i]);
+		}
 	}
 	
 
