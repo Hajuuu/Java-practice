66232a8b00a5171719fd06f99b40fea7244df05f
Juyeon Ha <hpt0127@naver.com>
2023-01-08 20:53:33 +0900

    MyMathTest

--- example/src/example/practice.java
+++ example/src/example/practice.java
@@ -1,33 +1,30 @@
 package example;
 
-class Card{
-	String kind;
-	int number;
-	static int width = 100;
-	static int height = 250;
-}
+
 public class practice {
 
 	public static void main(String[] args) {
-		System.out.println("Card.width = "+Card.width);
-		System.out.println("Card.height = "+Card.height);
+		MyMath mm = new MyMath();
+		long result1 = mm.add(5L, 3L);
+		long result2 = mm.subtract(5L, 3L);
+		long result3 = mm.multiply(5L, 3L);
+		double result4 = mm.divide(5L, 3L);
 		
-		Card c1 = new Card();
-		c1.kind = "Heart";
-		c1.number = 7;
+		System.out.println("add(5L, 3L) = "+result1);
+		System.out.println("subtract(5L, 3L) = "+result2);
+		System.out.println("multiply(5L, 3L) = "+result3);
+		System.out.println("divide(5L, 3L) = "+result4);
 		
-		Card c2 = new Card();
-		c2.kind = "Spade";
-		c2.number = 4;
 		
-		System.out.println("c1은 "+c1.kind+", "+c1.number+"이며,크기는 ("+c1.width+", "+c1.height+")");
-		System.out.println("c2는 "+c2.kind+", "+c2.number+"이며,크기는 ("+c2.width+", "+c2.height+")");
-		System.out.println("c1의 width와 height를 각각 50, 80으로 변경한다.");
-		c1.width = 50;
-		c1.height = 80;
-		
-		System.out.println("c1은 "+c1.kind+", "+c1.number+"이며,크기는 ("+c1.width+", "+c1.height+")");
-		System.out.println("c2는 "+c2.kind+", "+c2.number+"이며,크기는 ("+c2.width+", "+c2.height+")");
 	}
 
+}
+
+class MyMath {
+	long add(long a, long b) { return a + b; }
+	long subtract(long a, long b) { return a - b; }
+	long multiply(long a, long b) { return a * b; }
+	double divide(double a, double b) { return a / b; }
+	
+	
 }
