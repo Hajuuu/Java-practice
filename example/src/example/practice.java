5caf4399b62a5cd542756f960e50a5fbab7a740b
Juyeon Ha <hpt0127@naver.com>
2023-01-08 19:43:33 +0900

    CardTest

--- example/src/example/practice.java
+++ example/src/example/practice.java
@@ -1,30 +1,33 @@
 package example;
 
-class Tv{
-	//Tv의 속성(멤버변수)
-	String color;
-	boolean power;
-	int channel;
-	
-	//Tv의 기능(메서드)
-	void power() {power = !power;}
-	void channelUp() {++channel;}
-	void channelDown() {--channel;}
+class Card{
+	String kind;
+	int number;
+	static int width = 100;
+	static int height = 250;
 }
 public class practice {
 
 	public static void main(String[] args) {
-		Tv[] tvArr  = new Tv[3];
+		System.out.println("Card.width = "+Card.width);
+		System.out.println("Card.height = "+Card.height);
 		
-		for(int i=0; i < tvArr.length; i++) {
-			tvArr[i] = new Tv();
-			tvArr[i].channel = i+10;
-		}
+		Card c1 = new Card();
+		c1.kind = "Heart";
+		c1.number = 7;
 		
-		for(int i=0; i < tvArr.length; i++) {
-			tvArr[i].channelUp();
-			System.out.printf("tvArr[%d].channel=%d%n",i,tvArr[i].channel);
-		}
+		Card c2 = new Card();
+		c2.kind = "Spade";
+		c2.number = 4;
+		
+		System.out.println("c1은 "+c1.kind+", "+c1.number+"이며,크기는 ("+c1.width+", "+c1.height+")");
+		System.out.println("c2는 "+c2.kind+", "+c2.number+"이며,크기는 ("+c2.width+", "+c2.height+")");
+		System.out.println("c1의 width와 height를 각각 50, 80으로 변경한다.");
+		c1.width = 50;
+		c1.height = 80;
+		
+		System.out.println("c1은 "+c1.kind+", "+c1.number+"이며,크기는 ("+c1.width+", "+c1.height+")");
+		System.out.println("c2는 "+c2.kind+", "+c2.number+"이며,크기는 ("+c2.width+", "+c2.height+")");
 	}
 
 }
