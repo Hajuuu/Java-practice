0b5a65e6551259ad0983ac9b410ac7e54a004565
Juyeon Ha <hpt0127@naver.com>
2023-01-08 22:02:14 +0900

    ReferenceParamEx3

--- example/src/example/practice.java
+++ example/src/example/practice.java
@@ -3,17 +3,40 @@ package example;
 public class practice {
 
 	public static void main(String[] args) {
-		int[] x = {100};
-		System.out.println("main() : x = "+x[0]);
+		int[] arr = new int[] {3, 2, 1, 6, 5, 4};
 		
-		change(x);
-		System.out.println("After change (d.x)");
-		System.out.println("main() : x = "+x[0]);
+		printArr(arr);
+		sortArr(arr);
+		printArr(arr);
+		System.out.println("sum="+sumArr(arr));
 	}
 	
-	static void change(int[] x) { // 참조형 매개변수
-		x[0] = 1000;
-		System.out.println("change () :  x = "+x[0]);
+	static void printArr(int[] arr) {
+		System.out.print("[");
+		for(int i : arr)
+			System.out.print(i+",");
+		System.out.println("]");
 	}
+	
+	static int sumArr(int[] arr) {
+		int sum = 0;
+		
+		for(int i = 0; i < arr.length; i++)
+			sum += arr[i];
+		return sum;
+	}
+	
+	static void sortArr(int[] arr) {
+		for(int i=0; i < arr.length-1; i++) {
+			for(int j=0; j < arr.length-1-i; j++) {
+				if(arr[j] > arr [j+1]) {
+					int tmp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = tmp;
+				}
+			}
+		}
+	}
+	
 
 }
