4f271bdbbc736ae096d5952ac3972a435eae7afa
Juyeon Ha <hpt0127@naver.com>
2023-01-08 21:50:09 +0900

    ReferenceParamEx2

--- example/src/example/practice.java
+++ example/src/example/practice.java
@@ -1,22 +1,19 @@
 package example;
 
-class Data { int x; }
-
 public class practice {
 
 	public static void main(String[] args) {
-		Data d = new Data();
-		d.x = 100;
-		System.out.println("main() : x = "+d.x);
+		int[] x = {100};
+		System.out.println("main() : x = "+x[0]);
 		
-		change(d);
+		change(x);
 		System.out.println("After change (d.x)");
-		System.out.println("main() : x = "+d.x);
+		System.out.println("main() : x = "+x[0]);
 	}
 	
-	static void change(Data d) { // 참조형 매개변수
-		d.x = 1000;
-		System.out.println("change () :  x = "+d.x);
+	static void change(int[] x) { // 참조형 매개변수
+		x[0] = 1000;
+		System.out.println("change () :  x = "+x[0]);
 	}
 
 }
