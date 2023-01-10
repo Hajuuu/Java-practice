a095e8358ee9dafada673b51edf622e4a8073e37
Juyeon Ha <hpt0127@naver.com>
2023-01-10 19:19:42 +0900

    PowerTest

--- example/src/example/practice.java
+++ example/src/example/practice.java
@@ -3,22 +3,20 @@ package example;
 public class practice {
 
 	public static void main(String[] args) {
-		int result = factorial(4);
+		int x = 2;
+		int n = 5;
+		long result = 0;
+		
+		for(int i = 1; i <= n; i++) {
+			result += power(x, i);
+		}
 		
 		System.out.println(result);
 	}
 	
-	static int factorial(int n) {
-		int result = 0;
-		
-		if(n==1)
-			result = 1;
-		else
-			result = n * factorial(n-1);
-		
-		return result;
+	static long power(int x, int n) {
+		if(n==1) return x;
+		return x * power(x, n-1);
 	}
 	
-	
-
 }
