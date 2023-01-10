9695ffcbd2516cf7ffef2563853f4244c81f0f47
Juyeon Ha <hpt0127@naver.com>
2023-01-10 21:09:06 +0900

    OverloadingTest

--- example/src/example/practice.java
+++ example/src/example/practice.java
@@ -1,35 +1,47 @@
 package example;
 
-class MyMath2 {
-	long a, b;
-	
-	//인스턴스변수 a, b만을 이용해서 작업하므로 매개변수가 필요없다.
-	long add()	{ return a + b; }
-	long subtract()	{ return a - b; }
-	long multiply()	{ return a * b; }
-	double divide()	{ return a / b; }
-	
-	//인스턴스변수와 관계없이 매개변수만으로 작업이 가능하다.
-	static long add(long a, long b)	{ return a + b; }
-	static long subtract(long a, long b)	{ return a - b; }
-	static long multiply(long a, long b)	{ return a * b; }
-	static double divide(double a, double b)	{ return a / b; }
-	
-}
 public class practice {
 
 	public static void main(String[] args) {
-		System.out.println(MyMath2.add(200L, 100L));
-		System.out.println(MyMath2.subtract(200L, 100L));
-		System.out.println(MyMath2.multiply(200L, 100L));
-		System.out.println(MyMath2.divide(200L, 100L));
+		MyMath3 mm = new MyMath3();
+		System.out.println("mm.add(3, 3) 결과:"+mm.add(3,3));
+		System.out.println("mm.add(3L, 3) 결과:"+mm.add(3L,3));
+		System.out.println("mm.add(3, 3L) 결과:"+mm.add(3,3L));
+		System.out.println("mm.add(3L, 3L) 결과:"+mm.add(3L,3L));
 		
-		MyMath2 mm = new MyMath2();
-		mm.a = 200L;
-		mm.b = 100L;
-		System.out.println(mm.add());
-		System.out.println(mm.subtract());
-		System.out.println(mm.multiply());
-		System.out.println(mm.divide());
+		int[] a = {100, 200, 300};
+		System.out.println("mm.add(a) 결과:"+mm.add(a));
 	}
+}
+
+class MyMath3 {
+	int add(int a, int b) {
+		System.out.print("int add(int a, int b) - ");
+		return a+b;
+	}
+	
+	long add(int a, long b) {
+		System.out.print("long add(int a, long b) - ");
+		return a+b;
+	}
+	
+	long add(long a, int b) {
+		System.out.print("long add(long a, int b) - ");
+		return a+b;
+	}
+	
+	long add(long a, long b) {
+		System.out.print("long add(long a, long b) - ");
+		return a+b;
+	}
+	
+	int add(int[] a) {
+		System.out.print("int add(int[] a) - ");
+		int result = 0;
+		for(int i=0; i < a.length; i++) {
+			result += a[i];
+		}
+		return result;
+	}
+	
 }
