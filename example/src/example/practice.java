6ebbb89e0958ae5c4dd206b1f26a99d31c92f510
Juyeon Ha <hpt0127@naver.com>
2023-01-10 19:59:26 +0900

    MyMathTest2

--- example/src/example/practice.java
+++ example/src/example/practice.java
@@ -1,22 +1,35 @@
 package example;
 
+class MyMath2 {
+	long a, b;
+	
+	//인스턴스변수 a, b만을 이용해서 작업하므로 매개변수가 필요없다.
+	long add()	{ return a + b; }
+	long subtract()	{ return a - b; }
+	long multiply()	{ return a * b; }
+	double divide()	{ return a / b; }
+	
+	//인스턴스변수와 관계없이 매개변수만으로 작업이 가능하다.
+	static long add(long a, long b)	{ return a + b; }
+	static long subtract(long a, long b)	{ return a - b; }
+	static long multiply(long a, long b)	{ return a * b; }
+	static double divide(double a, double b)	{ return a / b; }
+	
+}
 public class practice {
 
 	public static void main(String[] args) {
-		int x = 2;
-		int n = 5;
-		long result = 0;
+		System.out.println(MyMath2.add(200L, 100L));
+		System.out.println(MyMath2.subtract(200L, 100L));
+		System.out.println(MyMath2.multiply(200L, 100L));
+		System.out.println(MyMath2.divide(200L, 100L));
 		
-		for(int i = 1; i <= n; i++) {
-			result += power(x, i);
-		}
-		
-		System.out.println(result);
+		MyMath2 mm = new MyMath2();
+		mm.a = 200L;
+		mm.b = 100L;
+		System.out.println(mm.add());
+		System.out.println(mm.subtract());
+		System.out.println(mm.multiply());
+		System.out.println(mm.divide());
 	}
-	
-	static long power(int x, int n) {
-		if(n==1) return x;
-		return x * power(x, n-1);
-	}
-	
 }
