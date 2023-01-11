27004a4fa8711d3d22137a7f1644d1c65614d950
Juyeon Ha <hpt0127@naver.com>
2023-01-11 19:40:10 +0900

    CarTest3

--- example/src/example/practice.java
+++ example/src/example/practice.java
@@ -1,19 +1,21 @@
 package example;
 
-class Car{
+class Car {
 	String color;
 	String gearType;
 	int door;
 	
-	Car(){
+	Car() {
 		this("white", "auto", 4);
 	}
 	
-	Car(String color){
-		this(color, "auto", 4);
+	Car(Car c) {
+		color = c.color;
+		gearType = c.gearType;
+		door = c.door;
 	}
 	
-	Car(String color, String gearType, int door){
+	Car(String color, String gearType, int door) {
 		this.color = color;
 		this.gearType = gearType;
 		this.door = door;
@@ -23,9 +25,12 @@ public class practice {
 
 	public static void main(String[] args) {
 		Car c1 = new Car();
-		Car c2 = new Car("blue");
-		
+		Car c2 = new Car(c1);
 		System.out.println("c1의 color="+c1.color+", gearType="+c1.gearType+", door="+c1.door);
-		System.out.println("c2의 color="+c2.color+", gearType="+c2.gearType+", door="+c2.door);
+		System.out.println("c2의 color="+c2.color+", gearType="+c1.gearType+", door="+c1.door);
+		c1.door = 100;
+		System.out.println("c1.door = 100; 수행 후");
+		System.out.println("c1의 color="+c1.color+", gearType="+c1.gearType+", door="+c1.door);
+		System.out.println("c2의 color="+c2.color+", gearType="+c1.gearType+", door="+c1.door);
 	}
 }
