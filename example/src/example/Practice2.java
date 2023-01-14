bac3c766a484d445f66efc20ef62f32010471e57
Juyeon Ha <hpt0127@naver.com>
2023-01-14 15:18:14 +0900

    PointTest2

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -3,34 +3,34 @@ package example;
 public class Practice2 {
 
 	public static void main(String[] args) {
-		
-		Point3D p3 = new Point3D(1,2,3);
+		Point3D p3 = new Point3D();
+		System.out.println("p3.x="+p3.x);
+		System.out.println("p3.y="+p3.y);
+		System.out.println("p3.z="+p3.z);
 	}
 
 }
 
+
 class Point {
-	int x, y;
+	int x = 10;
+	int y = 20;
 	
 	Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
-	
-	String getLocation() {
-		return "x: " + x + ", y: " + y;
-	}
 }
 
 class Point3D extends Point {
-	int z;
+	int z = 30;
+	
+	Point3D() {
+		this(100, 200, 300);
+	}
 	
 	Point3D(int x, int y, int z) {
 		super(x, y);
 		this.z = z;
-	}
-	
-	String getLocation() {
-		return "x: " + x + ", y: " + y + ", z: " + z;
 	}
 }
