ca7912425c4075b7bc3a26f85f65f8c7fae65179
Juyeon Ha <hpt0127@naver.com>
2023-01-15 12:31:18 +0900

    FinalCardTest

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -1,14 +1,32 @@
 package example;
 
-import static java.lang.System.out;
-import static java.lang.Math.*;
-
+class Card {
+	final int NUMBER;
+	final String KIND; // 생성자에서 단 한번만 초기화할 수 있다.
+	static int width = 100;
+	static int height = 250;
+	
+	Card(String kind, int num) {
+		KIND = kind;
+		NUMBER = num;
+	}
+	
+	Card() {
+		this("HEART", 1);
+	}
+	
+	public String toString() {
+		return KIND + " " + NUMBER;
+	}
+}
 public class Practice2 {
 
 	public static void main(String[] args) {
-		out.println(random());
-		
-		out.println("Math.PI :" + PI);
+		Card c = new Card("HEART", 10);
+		//c.NUMBER = 5; 
+		System.out.println(c.KIND);
+		System.out.println(c.NUMBER);
+		System.out.println(c);
 	}
 
 }
