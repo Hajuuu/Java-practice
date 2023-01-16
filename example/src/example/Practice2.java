db538edf718fb7bb722899d645cefd5077f1db76
Juyeon Ha <hpt0127@naver.com>
2023-01-16 18:30:52 +0900

    SingletonTest

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -1,43 +1,24 @@
 package example;
 
-
+final class Singleton {
+	private static Singleton s = new Singleton();
+	
+	private Singleton() {
+		
+	}
+	
+	public static Singleton getInstance() {
+		if(s==null) {
+			s = new Singleton();
+		}
+		return s;
+	}
+}
 public class Practice2 {
 
 	public static void main(String[] args) {
-		Time t = new Time(12, 35, 30);
-		System.out.println(t);
-		t.setHour(t.getHour() + 1);
-		System.out.println(t);
+		//Singleton s = new Singleton(); error, Singleton() has private access in Singleton
+		Singleton s = Singleton.getInstance();
 	}
 
-}
-
-class Time {
-	private int hour, minute, second;
-	
-	Time(int hour, int minute, int second) {
-		setHour(hour);
-		setMinute(minute);
-		setSecond(second);
-	}
-	
-	public int getHour() { return hour; }
-	public void setHour(int hour) {
-		if (hour < 0 || hour > 23) return;
-		this.hour = hour;
-	}
-	public int getMinute() { return minute; }
-	public void setMinute(int minute) {
-		if (minute < 0 || minute > 59) return;
-		this.minute = minute;
-	}
-	public int getSecond() { return second; }
-	public void setSecond(int second) {
-		if (second < 0 || second > 59) return;
-		this.second = second;
-	}
-	
-	public String toString() {
-		return hour + ":" + minute + ":" + second;
-	}
 }
