7bf1048ab63a2d1cbca1073e6ea8c9c071cc298d
Juyeon Ha <hpt0127@naver.com>
2023-01-16 20:28:31 +0900

    CastingTest2

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -3,15 +3,16 @@ package example;
 public class Practice2 {
 
 	public static void main(String[] args) {
-		Car car = null;
-		FireEngine fe = new FireEngine();
-		FireEngine fe2 = null;
+		Car car = new Car();
+		Car car2 = null;
+		FireEngine fe = null;
 		
-		fe.water();
-		car = fe;
-		//car.water(); error. Car 타입의 참조변수인 car로는 water()를 사용 불가.
-		fe2 = (FireEngine)car;
-		fe2.water();
+		car.drive();
+		fe = (FireEngine) car;
+		fe.drive();
+		car2 = fe;
+		car2.drive();
+		
 	}
 
 }
