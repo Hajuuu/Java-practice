b97260737881a4f3b727f6859e4e5e7687c974c6
Juyeon Ha <hpt0127@naver.com>
2023-01-17 20:06:19 +0900

    PolyArgumentTest

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -1,28 +1,57 @@
 package example;
 
+
+class Product {
+	int price;
+	int bonusPoint;
+	
+	Product(int price) {
+		this.price = price;
+		bonusPoint = (int) (price / 10.0);
+	}
+}
+
+class Tv extends Product {
+	Tv() {
+		super(100);
+	}
+	public String toString() { return "Tv"; }
+}
+
+class Computer extends Product {
+	Computer() { super(200); }
+	
+	public String toString() { return "Computer"; }
+	
+}
+
+class Buyer {
+	int money = 1000;
+	int bonusPoint = 0;
+	
+	void buy(Product p) {
+		if(money < p.price) {
+			System.out.println("잔액이 부족하여 물건을 살 수 없습니다.");
+			return;
+		}
+		
+		money -= p.price;
+		bonusPoint += p.bonusPoint;
+		System.out.println(p + "을/를 구입하셨습니다.");
+	}
+}
 public class Practice2 {
 
 	public static void main(String[] args) {
-		Parent p = new Child();
-		Child c = new Child();
+		Buyer b = new Buyer();
 		
-		System.out.println("p.x = " + p.x);
-		p.method();
+		//Product p = new Tv1();
+		//b.buy(p);
+		b.buy(new Tv());
+		b.buy(new Computer());
 		
-		System.out.println("c.x = " + c.x);
-		c.method();
+		System.out.println("현재 남은 돈은" + b.money + "만원입니다." );
+		System.out.println("현재 보너스점수는 " + b.bonusPoint + "점입니다.");
 	}
 
-}
-
-class Parent {
-	int x = 100;
-	
-	void method() {
-		System.out.println("Parent Method");
-	}
-}
-
-class Child extends Parent {
-	
 }
