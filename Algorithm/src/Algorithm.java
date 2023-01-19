f1b0b9fa9e649e3b7a74fd670bb40d3eb19f0584
Juyeon Ha <hpt0127@naver.com>
2023-01-19 20:00:06 +0900

    practice2-5

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -1,38 +1,38 @@
-import java.util.Random;
+import java.util.Arrays;
 import java.util.Scanner;
 
 
 public class Algorithm {
 
-	static int maxOf(int[] a) {
-		int max = a[0];
-		for(int i = 0; i < a.length; i++) {
-			if(a[i] > max)
-				max = a[i];
-		}
-		return max;
-	}
+	static void swap(int[] a, int idx1, int idx2) {
+		int t = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = t;
+	} 
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Random rand =new Random();
-		Scanner stdIn = new Scanner(System.in);
-		
-		System.out.println("키의 최댓값을 구합니다.");
-		int num = rand.nextInt(10);
-		System.out.println("사람 수: " + num);
-		
-		
-		int[] height = new int[num];
-		
-		System.out.println("키는 다음과 같습니다.");
-		for(int i = 0; i < num; i++) {
-			height[i] = 100 + rand.nextInt(90);
-			System.out.println("height[" + i + "]: " + height[i]);
+	static void reverse(int[] a) {
+		for(int i = 0; i < a.length / 2; i++) {
+			swap(a, i, a.length - i - 1);
 		}
+	}
+	public static void main(String[] args) {
 		
-		System.out.println("최대값은 " + maxOf(height) + "입니다.");
+			Scanner stdIn = new Scanner(System.in);
 			
+			System.out.print("요솟수: ");
+			int num = stdIn.nextInt();
+			
+			int[] x = new int[num];
+			
+			for(int i = 0; i < num; i++) {
+				System.out.print("x[" + i + "]: ");
+				x[i] = stdIn.nextInt();
+			}
+			
+			reverse(x);
+			
+			System.out.println("요소를 역순으로 정렬했습니다.");
+			System.out.println("x = " + Arrays.toString(x));
 	}
 
 }
