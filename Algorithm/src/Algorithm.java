d00dcadcc217f73e721c0919a13ec26150fec413
Juyeon Ha <hpt0127@naver.com>
2023-01-19 20:40:09 +0900

    practice2-6

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -4,35 +4,56 @@ import java.util.Scanner;
 
 public class Algorithm {
 
-	static void swap(int[] a, int idx1, int idx2) {
-		int t = a[idx1];
-		a[idx1] = a[idx2];
-		a[idx2] = t;
-	} 
-	
-	static void reverse(int[] a) {
-		for(int i = 0; i < a.length / 2; i++) {
-			swap(a, i, a.length - i - 1);
-		}
-	}
-	public static void main(String[] args) {
+	static int cardConv(int x, int r, char[] d) {
+		int digits = 0;
+		String dchar = "0123456789ABCDEFGHIJKLMNOOPQRSTUVWXYZ";
 		
-			Scanner stdIn = new Scanner(System.in);
+		do {
+			d[digits++] = dchar.charAt(x % r);
+			x /= r;
+		} while(x != 0);
+		
+		for(int i = 0; i < digits / 2; i++) {
+			char t = d[i];
+			d[i] = d[digits - i - 1];
+			d[digits - i - 1] = t;
+		}
+		
+		return digits;
+	}
+	
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		int no;
+		int cd;
+		int dno;
+		int retry;
+		char[] cno = new char[32];
+		
+		System.out.println("10진수를 기수 변환합니다.");
+		do {
+			do {
+				System.out.print("변환하는 음이 아닌 정수: ");
+				no = stdIn.nextInt();
+			} while(no < 0);
 			
-			System.out.print("요솟수: ");
-			int num = stdIn.nextInt();
+			do {
+				System.out.print("어떤 진수로 변환할까요? (2~36): ");
+				cd = stdIn.nextInt();
+			} while(cd < 2 || cd > 36);
 			
-			int[] x = new int[num];
+			dno = cardConv(no, cd, cno);
 			
-			for(int i = 0; i < num; i++) {
-				System.out.print("x[" + i + "]: ");
-				x[i] = stdIn.nextInt();
-			}
+			System.out.println(cd + "진수로");
+			for(int i = 0; i < dno; i++) {
+				System.out.print(cno[i]);
+			} System.out.println("입니다.");
 			
-			reverse(x);
+			System.out.print("한 번 더 할까요? (1: 예 2: 아니요): ");
+			retry = stdIn.nextInt();
 			
-			System.out.println("요소를 역순으로 정렬했습니다.");
-			System.out.println("x = " + Arrays.toString(x));
+			
+		} while(retry == 1);
 	}
 
 }
