0c2e8196289981a618bddd84041f07d4da192f64
Juyeon Ha <hpt0127@naver.com>
2023-01-19 20:59:25 +0900

    practice2-8

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -6,16 +6,24 @@ public class Algorithm {
 
 	public static void main(String[] args) {
 		int counter = 0;
+		int ptr = 0;
+		int[] prime = new int[500];
 		
-		for(int n = 2; n <= 1000; n++) {
+		prime[ptr++] = 2;
+		
+		for(int n = 3; n <= 1000; n += 2) {
 			int i;
-			for(i = 2; i < n; i++) {
+			for(i = 1; i < ptr; i++) {
 				counter++;
-				if(n % i == 0)
+				if(n % prime[i] == 0)
 					break;
 			}
-			if(n == i)
-				System.out.println(n);
+			if(ptr == i)
+				prime[ptr++] = n;
+		}
+		
+		for(int i = 0; i < ptr; i++) {
+			System.out.println(prime[i]);
 		}
 		
 		System.out.println("나눗셈을 수행한 횟수: " + counter);
