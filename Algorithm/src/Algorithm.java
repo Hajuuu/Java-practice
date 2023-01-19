b53c42e0edcb901c92515d8f19b412f3a2728b2d
Juyeon Ha <hpt0127@naver.com>
2023-01-19 19:10:30 +0900

    practice2-3

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -1,13 +1,34 @@
+import java.util.Scanner;
+
 
 public class Algorithm {
 
+	static int maxOf(int[] a) {
+		int max = a[0];
+		for(int i = 0; i < a.length; i++) {
+			if(a[i] > max)
+				max = a[i];		
+		}
+		return max;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			int[] a = {1, 2, 3, 4, 5};
+			Scanner stdIn = new Scanner(System.in);
 			
-			for(int i = 0; i < a.length; i++) {
-				System.out.println("a[" + i + "] = " + a[i]);
+			System.out.println("키의 최댓값을 구합니다.");
+			System.out.println("사람 수: ");
+			int num = stdIn.nextInt();
+			
+			int[] height = new int[num];
+			
+			for(int i = 0; i < num; i++) {
+				System.out.println("height[" + i + "]:");
+				height[i] = stdIn.nextInt();
 			}
+			
+			System.out.println("최댓값은" + maxOf(height) + "입니다. ");
+			
 	}
 
 }
