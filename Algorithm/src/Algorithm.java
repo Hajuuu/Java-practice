627413fe91eeb0dd60564d4fa8eefd03e9a14ecd
Juyeon Ha <hpt0127@naver.com>
2023-01-19 18:35:02 +0900

    practice2-1

---
+++ Algorithm/src/Algorithm.java
@@ -0,0 +1,17 @@
+
+public class Algorithm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int[] a = new int[5];
+			
+			a[1] = 37;
+			a[2] = 51;
+			a[4] = a[1] * 2;
+			
+			for(int i = 0; i < a.length; i++) {
+				System.out.println("a[" + i + "] = " + a[i]);
+			}
+	}
+
+}
