275fe20a6e9fb3b77d29e444b18226b635cccaec
Juyeon Ha <hpt0127@naver.com>
2023-01-21 21:51:06 +0900

    InterfaceTest2

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -1,14 +1,24 @@
 package example;
 
 class A {
-	public void methodA(B b) {
-		b.methodB();
+	public void method(I i) { // 인터페이스 I를 구현한 것만 매개변수 이용가능
+		i.method();
 	}
 }
 
-class B {
-	public void methodB() {
-		System.out.println("methodB()");
+interface I {
+	public void method();
+}
+
+class B implements I {
+	public void method() {
+		System.out.println("B클래스의 메서드");
+	}
+}
+
+class C {
+	public void method() {
+		System.out.println("C클래스의 메서드");
 	}
 }
 
@@ -16,7 +26,7 @@ public class Practice2 {
 
 	public static void main(String[] args) {
 		A a = new A();
-		a.methodA(new B());
+		a.method(new B());
 	}
 
 }
