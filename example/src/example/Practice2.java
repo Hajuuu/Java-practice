85cc906bcbb6d7c94400cd805363ad41210f0ea8
Juyeon Ha <hpt0127@naver.com>
2023-01-21 21:34:48 +0900

    InterfaceTest

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -1,42 +1,22 @@
 package example;
 
-interface Parseable {
-	public abstract void parse(String fileName);
-}
-
-class ParserManager {
-	public static Parseable getParser(String type) {
-		if(type.equals("XML")) {
-			return new XMLParser();
-		} else {
-			Parseable p = new HTMLParser();
-			return p;
-		}
+class A {
+	public void methodA(B b) {
+		b.methodB();
 	}
 }
 
-
-class XMLParser implements Parseable {
-	public void parse(String fileName) {
-		System.out.println(fileName + "- XML parsing completed.");
+class B {
+	public void methodB() {
+		System.out.println("methodB()");
 	}
 }
-
-class HTMLParser implements Parseable {
-	public void parse(String fileName) {
-		System.out.println(fileName + "-HTML parsing completed.");
-	}
-}
-
 
 public class Practice2 {
 
 	public static void main(String[] args) {
-		Parseable parser = ParserManager.getParser("XML");
-		parser.parse("document.xml");
-		parser = ParserManager.getParser("HTML");
-		parser.parse("document2.html");
-		
+		A a = new A();
+		a.methodA(new B());
 	}
 
 }
