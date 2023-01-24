e003b88e024eea6d5373aa827036a97b8ba2a095
Juyeon Ha <hpt0127@naver.com>
2023-01-24 17:24:15 +0900

    InnerEx1

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -1,39 +1,33 @@
 package example;
 
-
-
 public class Practice2 {
 
+	class InstanceInner {
+		int iv = 100;
+		//static int cv = 100; // 에러 ! static변수를 선언할 수 없음
+		final static int CONST = 100;
+	}
+	
+	static class StaticInner {
+		int iv = 200;
+		static int cv = 200;
+	}
+	
+	void myMethod() {
+		class LocalInner {
+			int iv = 300;
+	//		static int cv = 300; // 에러 ! static변수를 선언할 수 없음
+			final static int CONST = 300;
+		}
+		
+		int i = LocalInner.CONST;
+	}
+	
 	public static void main(String[] args) {
-		A a = new A();
-		a.methodA();
+		System.out.println(InstanceInner.CONST);
+		System.out.println(StaticInner.cv);
+		// System.out.println(LocalInner.CONST); // 에러. 지역 내부 클래스는 메서드 내에서만
 
 	}
 
-}
-
-class A {
-	void methodA() {
-		I i = InstanceManager.getInstance();
-		i.methodB();
-		System.out.println(i.toString()); // 모든 객체는 Object 클래스에 정의된 메서드를 가지고 있기에 허용
-	}
-	
-}
-
-interface I {
-	public abstract void methodB();
-}
-
-class B implements I {
-	public void methodB() {
-		System.out.println("methodB in B class");
-	}
-	
-	public String toString() { return "class B"; }
-}
-class InstanceManager {
-	public static I getInstance() {
-		return new B();
-	}
 }
