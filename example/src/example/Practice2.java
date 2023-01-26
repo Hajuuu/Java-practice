b3f0c65643ad3b74c62dc7975feca289e36d3ef6
Juyeon Ha <hpt0127@naver.com>
2023-01-26 20:38:01 +0900

    11660_구간합구하기

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -9,22 +9,33 @@ public class Practice2 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-		int suNo = Integer.parseInt(stringTokenizer.nextToken());
-		int quizNo = Integer.parseInt(stringTokenizer.nextToken());
+		int N = Integer.parseInt(stringTokenizer.nextToken());
+		int M = Integer.parseInt(stringTokenizer.nextToken());
 		
-		long[] S = new long[suNo + 1];
-		stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-		for(int i = 1; i <= suNo; i++) {
-			S[i] = S[i - 1] + Integer.parseInt(stringTokenizer.nextToken());
-		}
-		for(int q = 0; q < quizNo; q++) {
+		int A[][] = new int[N + 1][N + 1]; 
+		for(int i = 1; i <= N; i++) {
 			stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-			int i = Integer.parseInt(stringTokenizer.nextToken());
-			int j = Integer.parseInt(stringTokenizer.nextToken());
-			System.out.println(S[j] - S[i - 1]);
+			for(int j = 1; j <= N; j++) {
+				A[i][j] = Integer.parseInt(stringTokenizer.nextToken());
+			}
 		}
 		
+		int D[][] = new int[N + 1][N + 1];
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= N; j++) {
+				D[i][j] = D[i - 1][j] + D[i][j - 1] - D[i - 1][j - 1]+ A[i][j];
+			}
+		}
 		
+		for(int i = 0; i < M; i++) {
+			stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+			int x1 = Integer.parseInt(stringTokenizer.nextToken());
+			int y1 = Integer.parseInt(stringTokenizer.nextToken());
+			int x2 = Integer.parseInt(stringTokenizer.nextToken());
+			int y2 = Integer.parseInt(stringTokenizer.nextToken());
+			System.out.println(D[x2][y2] - D[x2][y1 - 1] - D[x1 - 1][y2] + D[x1 - 1][y1 - 1]);
+			
+		}
 	}
 	
 }
