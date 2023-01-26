43c2f8a6e6792e01bdca8960e795d5908291f72f
Juyeon Ha <hpt0127@naver.com>
2023-01-26 18:54:54 +0900

    11695_구간합구하기

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -1,33 +1,30 @@
 package example;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Practice2 {
-
-	class InstanceInner {
-		int iv = 100;
-		//static int cv = 100; // 에러 ! static변수를 선언할 수 없음
-		final static int CONST = 100;
-	}
 	
-	static class StaticInner {
-		int iv = 200;
-		static int cv = 200;
-	}
-	
-	void myMethod() {
-		class LocalInner {
-			int iv = 300;
-	//		static int cv = 300; // 에러 ! static변수를 선언할 수 없음
-			final static int CONST = 300;
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+		int suNo = Integer.parseInt(stringTokenizer.nextToken());
+		int quizNo = Integer.parseInt(stringTokenizer.nextToken());
+		
+		long[] S = new long[suNo + 1];
+		stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+		for(int i = 1; i <= suNo; i++) {
+			S[i] = S[i - 1] + Integer.parseInt(stringTokenizer.nextToken());
+		}
+		for(int q = 0; q < quizNo; q++) {
+			stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+			int i = Integer.parseInt(stringTokenizer.nextToken());
+			int j = Integer.parseInt(stringTokenizer.nextToken());
+			System.out.println(S[j] - S[i - 1]);
 		}
 		
-		int i = LocalInner.CONST;
+		
 	}
 	
-	public static void main(String[] args) {
-		System.out.println(InstanceInner.CONST);
-		System.out.println(StaticInner.cv);
-		// System.out.println(LocalInner.CONST); // 에러. 지역 내부 클래스는 메서드 내에서만
-
-	}
-
 }
