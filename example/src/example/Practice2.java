93299c88cf760012c64b0ccd207646658df6ef20
Juyeon Ha <hpt0127@naver.com>
2023-01-29 23:45:14 +0900

    ExceptionEx2

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -1,31 +1,14 @@
 package example;
-import java.io.IOException;
-import java.util.Scanner;
 
 public class Practice2 {
 	
-	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int sum = 1;
-		int count = 1;
-		int end_index = 1;
-		int start_index = 1;
+	public static void main(String[] args) {
+		int number = 100;
+		int result = 0;
 		
-		while(end_index != N) {
-			if(sum == N) {
-				count++;
-				end_index++;
-				sum = sum + end_index;
-			}
-			else if(sum > N) {
-				sum = sum - start_index;
-				start_index++;
-			}
-			else {
-				end_index++;
-				sum = sum + end_index;
-			}
+		for(int i = 0; i < 10; i++) {
+			result = number / (int)(Math.random() * 10);
+			System.out.println(result);
 		}
 	}
 	
