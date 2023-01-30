477203d971d994692e8072993fd9507ab51e0694
Juyeon Ha <hpt0127@naver.com>
2023-01-30 21:04:38 +0900

    P12891_DNA비밀번호

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -6,44 +6,103 @@ import java.util.StringTokenizer;
 
 
 public class Algorithm {
-
-	public static void main(String[] args) throws IOException {
+	static int[] myArr;
+	static int[] checkArr;
+	static int checkSecret;
 	
+	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(bf.readLine());
-		int Result = 0;
-		long A[] = new long[N];
 		StringTokenizer st = new StringTokenizer(bf.readLine());
-		for(int i = 0; i < N; i++) {
-			A[i] = Long.parseLong(st.nextToken());
-		}
-		Arrays.sort(A);
-		for(int k = 0; k < N; k++) {
-			long find = A[k];
-			int i = 0;
-			int j = N - 1;
-			while(i < j) {
-				if(A[i] + A[j] == find) {
-					if(i != k && j != k) {
-						Result++;
-						break;
-					} else if(i == k) {
-						i++;
-					} else if(j == k){
-						j--;
-					}
-				}
-				else if(A[i] + A[j] > find) {
-					j--;
-				} else {
-					i++;
-				}
+		int S = Integer.parseInt(st.nextToken());
+		int P = Integer.parseInt(st.nextToken());
+		char[] A = new char[S];
+		int result = 0;
+		
+		myArr = new int[4];
+		checkArr = new int[4];
+		checkSecret = 0;
+		A = bf.readLine().toCharArray();
+		st = new StringTokenizer(bf.readLine());
+		for(int i = 0; i < 4; i++) {
+			checkArr[i] = Integer.parseInt(st.nextToken());
+			if(checkArr[i] == 0) {
+				checkSecret++;
 			}
-			
+		}
+		for(int i = 0; i < P; i++) {
+			Add(A[i]);
+		}
+		if(checkSecret == 4) {
+			result++;
 		}
 		
-		System.out.println(Result);
-		bf.close();
+		for(int i = P; i < S; i++) {
+			int j = i - P;
+			Add(A[i]);
+			Remove(A[j]);
+			if(checkSecret == 4 ) {
+				result++;
+			}
+		}
+		System.out.println(result);
+	}
+	
+	private static void Add(char c) {
+		switch (c) {
+		case 'A':
+			myArr[0]++;
+			if(myArr[0] == checkArr[0]) {
+				checkSecret++;
+			}
+			break;
+		case 'C':
+			myArr[1]++;
+			if(myArr[1] == checkArr[1]) {
+				checkSecret++;
+			}
+			break;
+		case 'G':
+			myArr[2]++;
+			if(myArr[2] == checkArr[2]) {
+				checkSecret++;
+			}
+			break;
+		case 'T':
+			myArr[3]++;
+			if(myArr[3] == checkArr[3]) {
+				checkSecret++;
+			}
+			break;
+		}
+	}
+	
+	private static void Remove(char c) {
+		switch (c) {
+		case 'A':
+			if(myArr[0] == checkArr[0]) {
+				checkSecret--;
+			}
+			myArr[0]--;
+			break;
+		case 'C':
+			if(myArr[1] == checkArr[1]) {
+				checkSecret--;
+			}
+			myArr[1]--;
+			break;
+		case 'G':
+			if(myArr[2] == checkArr[2]) {
+				checkSecret--;
+			}
+			myArr[2]--;
+			break;
+		case 'T':
+			if(myArr[3] == checkArr[3]) {
+				checkSecret++;
+			}
+			myArr[3]--;
+			break;
+		}
 	}
 
 }
