d1fa428fe8ad9bcb035aff774e1366c68fd049c3
Juyeon Ha <hpt0127@naver.com>
2023-01-30 19:40:07 +0900

    P1253_좋은수

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -8,31 +8,41 @@ import java.util.StringTokenizer;
 public class Algorithm {
 
 	public static void main(String[] args) throws IOException {
+	
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(bf.readLine());
-		int M = Integer.parseInt(bf.readLine());
-		int[] A = new int[N];
+		int Result = 0;
+		long A[] = new long[N];
 		StringTokenizer st = new StringTokenizer(bf.readLine());
 		for(int i = 0; i < N; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
+			A[i] = Long.parseLong(st.nextToken());
 		}
 		Arrays.sort(A);
-		int count = 0;
-		int i = 0;
-		int j = N - 1;
-		while(i < j) {
-			if(A[i] + A[j] < M) {
-				i++;
-			} else if(A[i] + A[j] > M) {
-				j--;
-			} else {
-				count++;
-				i++;
-				j--;
+		for(int k = 0; k < N; k++) {
+			long find = A[k];
+			int i = 0;
+			int j = N - 1;
+			while(i < j) {
+				if(A[i] + A[j] == find) {
+					if(i != k && j != k) {
+						Result++;
+						break;
+					} else if(i == k) {
+						i++;
+					} else if(j == k){
+						j--;
+					}
+				}
+				else if(A[i] + A[j] > find) {
+					j--;
+				} else {
+					i++;
+				}
 			}
+			
 		}
 		
-		System.out.println(count);
+		System.out.println(Result);
 		bf.close();
 	}
 
