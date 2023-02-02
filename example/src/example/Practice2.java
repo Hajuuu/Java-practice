04dfd39cb5aca8e9c0285dc7ea6dc5e4247a595e
Juyeon Ha <hpt0127@naver.com>
2023-02-02 18:58:39 +0900

    comparable_Ex

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -5,33 +5,27 @@ import java.util.*;
 public class Practice2 {
 	
 	public static void main(String[] args) {
-		ArrayList list = new ArrayList();
-		list.add("1");
-		list.add("2");
-		list.add("3");
-		list.add("4");
-		list.add("5");
+		String[] strArr = {"cat", "Dog", "lion", "tiger"};
 		
-		Iterator it = list.iterator();
+		Arrays.sort(strArr);
+		System.out.println("strArr=" + Arrays.toString(strArr));
 		
-		while(it.hasNext()) {
-			Object obj = it.next();
-			System.out.println(obj);
-		}
+		Arrays.sort(strArr, String.CASE_INSENSITIVE_ORDER);
+		System.out.println("strArr=" + Arrays.toString(strArr));
 		
-		//iterator는 1회용이라 다 쓰고나면 다시 얻어와야 한다.
-//		it = list.iterator();
-//		
-//		while(it.hasNext()) {
-//			Object obj = it.next();
-//			System.out.println(obj);
-//		}
-		
-		for(int i = 0; i < list.size(); i++) {
-			Object obj = list.get(i);
-			System.out.println(obj);
-		}
-		
+		Arrays.sort(strArr, new Descending());
+		System.out.println("strArr=" + Arrays.toString(strArr));
 	}
 	
+}
+
+class Descending implements Comparator {
+	public int compare(Object o1, Object o2) {
+		if(o1 instanceof Comparable && o2 instanceof Comparable) {
+			Comparable c1 = (Comparable) o1;
+			Comparable c2 = (Comparable) o2;
+			return c1.compareTo(c2) * -1;
+		}
+		return -1;
+	}
 }
