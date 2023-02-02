e76a7ac6d2ee5d098a4cb5f4e52c907a825c1207
Juyeon Ha <hpt0127@naver.com>
2023-02-02 19:36:19 +0900

    P11268_절댓값힙

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -6,17 +6,27 @@ public class Algorithm {
 	
 	
 	public static void main(String[] args) throws IOException{
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		Queue<Integer> myQueue = new LinkedList();
-		for(int i = 1; i <= n; i++) {
-			myQueue.add(i);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> myQueue = new PriorityQueue<>((o1, o2) ->  {
+			int first_abs = Math.abs(o1);
+			int second_abs = Math.abs(o2);
+			if(first_abs == second_abs)
+				return o1 > o2 ? 1 : -1;
+			else
+				return first_abs - second_abs;
+		});
+		for(int i = 0; i < N; i++) {
+			int request = Integer.parseInt(br.readLine());
+			if(request == 0) {
+				if(myQueue.isEmpty())
+					System.out.println("0");
+				else
+					System.out.println(myQueue.poll());
+			} else {
+				myQueue.add(request);
+			}
 		}
-		while(myQueue.size() > 1) {
-			myQueue.poll();
-			myQueue.add(myQueue.poll());
-		}
-		System.out.println(myQueue.poll());
 		
 	}
 
