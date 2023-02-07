514b512a9c41fa1dd6c68313f86caf8e1a97486e
Juyeon Ha <hpt0127@naver.com>
2023-02-07 19:22:40 +0900

    Ex10_1(1)

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -9,6 +9,7 @@ public class Practice2 {
 		System.out.println("이 해의 년도: "+today.get(Calendar.YEAR));
 		System.out.println("월(0~11): "+today.get(Calendar.MONTH));
 		System.out.println("이 달의 몇 일: "+today.get(Calendar.DATE));
+		System.out.println("이 달의 마지막 날: "+today.getActualMaximum(Calendar.DATE));
 	}
 	
 }
