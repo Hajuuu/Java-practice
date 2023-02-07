bfb83a4fafed6a865b7312a8461389109a62b1a1
Juyeon Ha <hpt0127@naver.com>
2023-02-07 20:15:23 +0900

    Ex10_3

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -5,24 +5,35 @@ import java.util.*;
 public class Practice2 {
 	
 	public static void main(String[] args) {
-		final String[] DAY_OF_WEEK = {"", "일", "월", "화", "수", "목", "금", "토"};
+		final int[] TIME_UNIT = {3600, 60, 1};
+		final String[] TIME_UNIT_NAME = {"시간", "분", "초"};
+		Calendar time1 = Calendar.getInstance();
+		Calendar time2 = Calendar.getInstance();
 		
-		Calendar date1 = Calendar.getInstance();
-		Calendar date2 = Calendar.getInstance();
+		time1.set(Calendar.HOUR_OF_DAY, 10);
+		time1.set(Calendar.MINUTE, 20);
+		time1.set(Calendar.SECOND, 30);
 		
-		date1.set(2012, 0, 27);
-		System.out.println("date1은" + toString(date1) + DAY_OF_WEEK[date1.get(Calendar.DAY_OF_WEEK)]+"요일이고");
-		System.out.println("오늘(date2)은" + toString(date2) + DAY_OF_WEEK[date2.get(Calendar.DAY_OF_WEEK)]+"요일입니다.");
+		time2.set(Calendar.HOUR_OF_DAY, 20);
+		time2.set(Calendar.MINUTE, 30);
+		time2.set(Calendar.SECOND, 10);
 		
-		long diffrence = (date2.getTimeInMillis() - date1.getTimeInMillis()) / 1000;
-		System.out.println(diffrence+"초가 지났습니다.");
-		System.out.println(diffrence/(24*60*60)+"일이 지났습니다.");
+		System.out.println("time1 :"+time1.get(Calendar.HOUR_OF_DAY)+"시"
+				+time1.get(Calendar.MINUTE)+"분"+time1.get(Calendar.SECOND)+"초");
 		
-	}
-
-	private static String toString(Calendar date) {
-		// TODO Auto-generated method stub
-		return date.get(Calendar.YEAR) + "년" + (date.get(Calendar.MONTH)+1) + "월" + date.get(Calendar.DATE) + "일";
+		System.out.println("time2 :"+time2.get(Calendar.HOUR_OF_DAY)+"시"
+				+time2.get(Calendar.MINUTE)+"분"+time2.get(Calendar.SECOND)+"초");
+		
+		long difference = Math.abs(time2.getTimeInMillis() - time1.getTimeInMillis())/1000;
+		System.out.println("time1과 time2의 차이는 "+difference+"초 입니다.");
+		
+		String tmp = "";
+		for(int i = 0; i < TIME_UNIT.length; i++) {
+			tmp += difference/TIME_UNIT[i] + TIME_UNIT_NAME[i];
+			difference %= TIME_UNIT[i];
+		}
+		
+		System.out.println("시분초로 변환하면 " + tmp + "입니다.");
 	}
 	
 }
