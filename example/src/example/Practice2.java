b6ea472a69d2cb9138bc675ce29c54fb1c43550b
Juyeon Ha <hpt0127@naver.com>
2023-02-07 19:20:49 +0900

    Ex10_1

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -5,19 +5,10 @@ import java.util.*;
 public class Practice2 {
 	
 	public static void main(String[] args) {
-		Integer i = new Integer(100);
-		Integer i2 = new Integer(100);
-		
-		System.out.println("i==i2 ? "+(i==i2));
-		System.out.println("i.equals(i2) ? "+i.equals(i2));
-		System.out.println("i.compareTo(i2)= ? "+i.compareTo(i2));
-		System.out.println("i.toString()= ? "+i.toString());
-		
-		System.out.println("MAX_VALUE="+Integer.MAX_VALUE);
-		System.out.println("MIN_VALUE="+Integer.MIN_VALUE);
-		System.out.println("SIZE="+Integer.SIZE+" bits");
-		System.out.println("BYTES="+Integer.BYTES+" bytes");
-		System.out.println("TYPE="+Integer.TYPE);
+		Calendar today = Calendar.getInstance();
+		System.out.println("이 해의 년도: "+today.get(Calendar.YEAR));
+		System.out.println("월(0~11): "+today.get(Calendar.MONTH));
+		System.out.println("이 달의 몇 일: "+today.get(Calendar.DATE));
 	}
 	
 }
