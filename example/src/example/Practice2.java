1906adbdff7dbf600131ff5ebcc55f8d2ba2ce44
Juyeon Ha <hpt0127@naver.com>
2023-02-07 21:36:42 +0900

    Ex10_8

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -1,25 +1,37 @@
 package example;
 
-import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Practice2 {
 	
 	public static void main(String[] args) {
-		DecimalFormat df = new DecimalFormat("#,###.##");
-		DecimalFormat df2 = new DecimalFormat("#.###E0");
+		Date today = new Date();
 		
-		try {
-			Number num = df.parse("1,234,567.89");
-			System.out.print("1,234,567.89" + " -> ");
-			
-			double d = num.doubleValue();
-			System.out.print(d + " -> ");
-			
-			System.out.println(df2.format(num));
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		SimpleDateFormat sdf1, sdf2, sdf3, sdf4;
+		SimpleDateFormat sdf5, sdf6, sdf7, sdf8;
+		
+		sdf1 = new SimpleDateFormat("yyyy-MM-dd");
+		sdf2 = new SimpleDateFormat("''yy년 MMM dd일 E요일");
+		sdf3 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+		sdf4 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss a");
+		
+		sdf5 = new SimpleDateFormat("오늘은 올 해의 D번째 날입니다.");
+		sdf6 = new SimpleDateFormat("오늘은 이 달의 d번째 날입니다.");
+		sdf7 = new SimpleDateFormat("오늘은 올 해의 w번째 주입니다.");
+		sdf8 = new SimpleDateFormat("오늘은 이 달의 F번째 E요일입니다.");
+		
+		
+		System.out.println(sdf1.format(today));
+		System.out.println(sdf2.format(today));
+		System.out.println(sdf3.format(today));
+		System.out.println(sdf4.format(today));
+		System.out.println(sdf5.format(today));
+		System.out.println(sdf6.format(today));
+		System.out.println(sdf7.format(today));
+		System.out.println(sdf8.format(today));
+		
+		
 		
 	}
 
