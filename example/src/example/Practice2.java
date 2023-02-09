30e821b2afd562bbd0c664bacc15ddcafdb295a9
Juyeon Ha <hpt0127@naver.com>
2023-02-09 21:18:09 +0900

    Ex11_4

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -3,27 +3,55 @@ package example;
 import java.util.*;
 
 public class Practice2 {
+	static Queue q = new LinkedList();
+	static final int MAX_SIZE = 5;
 	
 	public static void main(String[] args) {
-		Stack st = new Stack();
-		Queue q = new LinkedList();
+		System.out.println("help를 입력하면 도움말을 볼 수 있습니다.");
 		
-		st.push("0");
-		st.push("1");
-		st.push("2");
-		
-		q.offer("0");
-		q.offer("1");
-		q.offer("2");
-		
-		System.out.println("= Stack =");
-		while(!st.isEmpty()) {
-			System.out.println(st.pop());
+		while(true) {
+			System.out.print(">>");
+			try {
+				Scanner s = new Scanner(System.in);
+				String input = s.nextLine().trim();
+				
+				if("".equals(input)) continue;
+				
+				if(input.equalsIgnoreCase("q")) {
+					System.exit(0);
+				} else if(input.equalsIgnoreCase("help")) {
+					System.out.println(" help - 도움말을 보여줍니다.");
+					System.out.println(" q 또는 Q - 프로그램을 종료합니다.");
+					System.out.println(" history - 최근에 입력한 명령어를" + MAX_SIZE + "개 보여줍니다.");
+				} else if(input.equalsIgnoreCase("history")) {
+					save(input);
+					
+					LinkedList list = (LinkedList)q;
+					
+					final int SIZE = list.size();
+					for(int i = 0; i < SIZE; i++) {
+						System.out.println((i+1)+"."+list.get(i));
+					}
+				} else {
+					save(input);
+					System.out.println(input);
+				}
+			} catch(Exception e) {
+				System.out.println("입력오류입니다.");
+			}
 		}
-		System.out.println("= Queue =");
-		while(!q.isEmpty()) {
-			System.out.println(q.poll());
-		}
+		
+		
+	}
+	
+	public static void save(String input) {
+		//queue에 저장한다.
+		if(!"".equals(input)) //if(input!=null && !input.equals(""))
+			q.offer(input);
+		
+		//queue의 최대크기를 넘으면 제일 먼저 넣은 것 삭제
+		if(q.size() > MAX_SIZE)
+			q.remove();
 	}
 	
 }
