f2bfd0093df92116d9882557fa5f940a2e65eed0
Juyeon Ha <hpt0127@naver.com>
2023-02-10 20:53:48 +0900

    Ex11_11

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -5,16 +5,42 @@ import java.util.*;
 public class Practice2 {
 	
 	public static void main(String[] args) {
-		Set set = new HashSet();
+		HashSet set = new HashSet();
 		
-		for(int i = 0; set.size() < 6; i++) {
-			int num = (int)(Math.random()*45) + 1;
-			set.add(num);
-		}
+		set.add("abc");
+		set.add("abc");
+		set.add(new Person("David", 10));
+		set.add(new Person("David", 10));
 		
-	//	System.out.println(set);
-		List list = new LinkedList(set);
-		Collections.sort(list);
-		System.out.println(list);
+		System.out.println(set);
+	}
+}
+
+class Person {
+	String name;
+	int age;
+	
+	Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public String toString() {
+		return name+":"+age;
+	}
+
+	@Override
+	public int hashCode() {
+		//가변인자
+		return Objects.hash(name, age);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof Person)) return false;
+		
+		Person p = (Person)obj;
+		
+		return this.name.equals(p.name) && this.age == p.age;
 	}
 }
