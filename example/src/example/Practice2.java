2c6dc3083a36fca4c5abe2c0d97b6a788eb1ffb6
Juyeon Ha <hpt0127@naver.com>
2023-02-10 20:22:35 +0900

    Ex11_9

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -5,26 +5,20 @@ import java.util.*;
 public class Practice2 {
 	
 	public static void main(String[] args) {
-		String[] strArr = {"cat", "Dog", "lion", "tiger"};
+		Object[] objArr = {"1", new Integer(1), "2", "2", "3", "3", "3", "4", "4", "4"};
+		Set set = new HashSet();
 		
-		Arrays.sort(strArr);
-		System.out.println("strArr="+Arrays.toString(strArr));
-		
-		Arrays.sort(strArr, String.CASE_INSENSITIVE_ORDER);
-		System.out.println("strArr="+Arrays.toString(strArr));
-		
-		Arrays.sort(strArr, new Descending());
-		System.out.println("strArr=" + Arrays.toString(strArr));
-	}
-}
-
-class Descending implements Comparator {
-	public int compare(Object o1, Object o2) {
-		if(o1 instanceof Comparable && o2 instanceof Comparable) {
-			Comparable c1 = (Comparable)o1;
-			Comparable c2 = (Comparable)o2;
-			return c1.compareTo(c2) * -1;
+		for(int i = 0; i < objArr.length; i++) {
+			System.out.println(objArr[i]+"="+set.add(objArr[i]));
 		}
-		return -1;
+		//HashSet에 저장된 요소들을 출력
+		System.out.println(set);
+		
+		//HashSet에 저장된 요소들을 출력(Iterator이용)
+		Iterator it = set.iterator();
+		
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
 	}
 }
