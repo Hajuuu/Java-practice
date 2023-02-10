225d7d5c2a134c8856ec743a8593c5fe2b9dda51
Juyeon Ha <hpt0127@naver.com>
2023-02-10 20:39:16 +0900

    Ex11_10

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -5,20 +5,16 @@ import java.util.*;
 public class Practice2 {
 	
 	public static void main(String[] args) {
-		Object[] objArr = {"1", new Integer(1), "2", "2", "3", "3", "3", "4", "4", "4"};
 		Set set = new HashSet();
 		
-		for(int i = 0; i < objArr.length; i++) {
-			System.out.println(objArr[i]+"="+set.add(objArr[i]));
+		for(int i = 0; set.size() < 6; i++) {
+			int num = (int)(Math.random()*45) + 1;
+			set.add(num);
 		}
-		//HashSet에 저장된 요소들을 출력
-		System.out.println(set);
 		
-		//HashSet에 저장된 요소들을 출력(Iterator이용)
-		Iterator it = set.iterator();
-		
-		while(it.hasNext()) {
-			System.out.println(it.next());
-		}
+	//	System.out.println(set);
+		List list = new LinkedList(set);
+		Collections.sort(list);
+		System.out.println(list);
 	}
 }
