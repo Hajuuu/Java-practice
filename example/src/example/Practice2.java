251ed4fc82f5762ae3378bfb748737e9cc8b7ead
Juyeon Ha <hpt0127@naver.com>
2023-02-11 16:09:35 +0900

    Ex11_13

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -5,53 +5,33 @@ import java.util.*;
 public class Practice2 {
 	
 	public static void main(String[] args) {
-		HashSet setA = new HashSet();
-		HashSet setB = new HashSet();
-		HashSet setHab = new HashSet();
-		HashSet setKyo = new HashSet();
-		HashSet setCha = new HashSet();
+		Set set = new TreeSet(); //범위 검색, 정렬. 정렬 필요없음
 		
-		setA.add("1");
-		setA.add("2");
-		setA.add("3");
-		setA.add("4");
-		setA.add("5");
-		System.out.println("A = "+setA);
-		
-		setB.add("4");
-		setB.add("5");
-		setB.add("6");
-		setB.add("7");
-		setB.add("8");
-		System.out.println("B = "+setB);
-		
-		Iterator it = setB.iterator();
-		while(it.hasNext()) {
-			Object tmp = it.next();
-			if(setA.contains(tmp))
-				setKyo.add(tmp);
-		}
-		
-		it = setA.iterator();
-		while(it.hasNext()) {
-			Object tmp = it.next();
-			if(!setB.contains(tmp))
-				setCha.add(tmp);
-		}
-		
-		it = setA.iterator();
-		while(it.hasNext())
-			setHab.add(it.next());
-		it = setB.iterator();
-		while(it.hasNext())
-			setHab.add(it.next());
-		
-		//setA.retainAll(setB); 교집합, 공통된 요소만 남기고 삭제
-		//setA.addAll(setB); 합집합, setB의 모든 요소를 추가(중복 제외)
-		//setA.removeAll(setB); 차집합, setB와 공통 요소를 제거
-		System.out.println("A ∪  B = " + setHab);
-		System.out.println("A ∩ B = " + setKyo);
-		System.out.println("A - B = " + setCha);
+//		for(int i = 0; set.size() < 6; i++) {
+//			int num = (int)(Math.random()*45) + 1;
+			set.add(new Test());
+//		}
+		System.out.println(set);
 	}
 }
 
+//객체가 비교 기준을 가지고 있거나 treeset이 비교 기준을 갖고있어야함.
+
+class Test implements Comparable {
+
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
+
+class TestComp implements Comparator {
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		return 0;
+	}
+	
+}
