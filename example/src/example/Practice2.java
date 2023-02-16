9c8c40306089eff998d9df5bbe56c229e148810e
Juyeon Ha <hpt0127@naver.com>
2023-02-17 00:05:29 +0900

    ch12_3

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -3,13 +3,30 @@ package example;
 import java.util.*;
 import static java.util.Collections.*;
 
-class Tv {}
-class Audio {}
+class Product {}
+class Tv extends Product {}
+class Audio extends Product {}
+
 public class Practice2 {
 	
 	public static void main(String[] args) {
-		ArrayList<Tv> list = new ArrayList<Tv>(); //참조 변수에 대입된 타입과 생성자에 대입된 타입이 같아야함
-		list.add(new Tv());
-		//list.add(new Audio());
+		ArrayList<Product> productList = new ArrayList<Product>();
+		ArrayList<Tv> tvList = new ArrayList<Tv>();
+//		ArrayList<Product> tvList = new ArrayList<Tv>(); //에러
+//		List<Tv> tvList = new ArrayList<Tv>(); //ok. 다형성
+		
+		productList.add(new Tv());
+		productList.add(new Audio());
+		
+		tvList.add(new Tv());
+		tvList.add(new Tv());
+		
+		printAll(productList);
+//		printAll(tvList); 컴파일 에러가 발생한다.
+	}
+	
+	public static void printAll(ArrayList<Product> list) {
+		for(Product p : list)
+			System.out.println(p);
 	}
 }
