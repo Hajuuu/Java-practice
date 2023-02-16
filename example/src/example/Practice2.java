8e72fcc23e1c94df7a9704b592946eb6e0752e4b
Juyeon Ha <hpt0127@naver.com>
2023-02-16 23:03:47 +0900

    ch12_2

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -3,19 +3,13 @@ package example;
 import java.util.*;
 import static java.util.Collections.*;
 
+class Tv {}
+class Audio {}
 public class Practice2 {
 	
 	public static void main(String[] args) {
-		//ArrayList list = new ArrayList(); //JDK1.5이전, 지네릭스 도입 이전
-		//ArrayList<Object> list = new ArrayList<Object>(); //지네릭스 도입 이후
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		list.add(10);
-		list.add(20);
-		//list.add("30"); 컴파일 에러
-		list.add(30);
-		
-		//Integer i = (Integer)list.get(2); //컴파일 ok, 형변환에러(실행시 발생 에러)
-		Integer i = list.get(2); //형변환 생략 가능
-		System.out.println(i);
+		ArrayList<Tv> list = new ArrayList<Tv>(); //참조 변수에 대입된 타입과 생성자에 대입된 타입이 같아야함
+		list.add(new Tv());
+		//list.add(new Audio());
 	}
 }
