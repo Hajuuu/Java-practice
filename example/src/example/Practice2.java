948c2127a3ed85a7893ccdba5c150b573c0b0583
Juyeon Ha <hpt0127@naver.com>
2023-02-17 15:06:47 +0900

    ch12_6

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -1,34 +1,45 @@
 package example;
 
 import java.util.*;
-import static java.util.Collections.*;
+
+class Fruit implements Eatable {
+	public String toString() { return "Fruit"; }
+}
+class Apple extends Fruit { public String toString() { return "Apple"; }}
+class Grape extends Fruit { public String toString() { return "Grape"; }}
+class Toy { public String toString() { return "Toy"; }}
+
+interface Eatable {}
 
 public class Practice2 {
 	
 	public static void main(String[] args) {
-		HashMap<String, Student> map = new HashMap<>();
-		map.put("자바왕", new Student("자바왕", 1, 1, 100, 100, 100));
+		FruitBox<Fruit> fruitBox = new FruitBox<Fruit>();
+		FruitBox<Apple> appleBox = new FruitBox<Apple>();
+		FruitBox<Grape> grapeBox = new FruitBox<Grape>();
+//		FruitBox<Grape> grapeBox = new FruitBox<Apple>(); // 에러. 타입 불일치
+//		FruitBox<Toy> toyBox = new FruitBox<Toy>(); // 에러
 		
-		Student s = map.get("자바왕");
+		fruitBox.add(new Fruit());
+		fruitBox.add(new Apple());
+		fruitBox.add(new Grape());
+		appleBox.add(new Apple());
+//		appleBox.add(new Grape()); // 에러. Grape는 Apple의 자손이 아님
+		grapeBox.add(new Grape());
 		
-		System.out.println(s.name);
+		System.out.println("fruitBox-"+fruitBox);
+		System.out.println("appleBox-"+appleBox);
+		System.out.println("grapeBox-"+grapeBox);
 	}
 }
 
-class Student {
-	String name = "";
-	int ban;
-	int no;
-	int kor;
-	int eng;
-	int math;
-	
-	Student(String name, int ban, int no, int kor, int eng, int math) {
-		this.name = name;
-		this.ban = ban;
-		this.no = no;
-		this.kor = kor;
-		this.eng = eng;
-		this.math = math;
-	}
+class FruitBox<T extends Fruit & Eatable> extends Box<T> {}
+
+class Box<T> {
+	ArrayList<T> list = new ArrayList<T>(); // item을 저장할 list
+	void add(T item) { list.add(item); } // 박스에 item을 추가
+	T get(int i) { return list.get(i); } // 박스에서 item을 꺼낼떄
+	int size() { return list.size(); }
+	public String toString() { return list.toString(); }
 }
+
