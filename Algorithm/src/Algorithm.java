2c87a2a01fd049d29937cdd471844eb0ef469d7d
Juyeon Ha <hpt0127@naver.com>
2023-02-28 01:11:56 +0900

    boj_10951

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -7,17 +7,17 @@ public class Algorithm {
 		
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
+		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		while(A != 0 && B != 0) {
+		String str;
+		while((str = br.readLine()) != null) {
+			st = new StringTokenizer(str, " ");
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
 			sb.append(A + B).append("\n");
-			st = new StringTokenizer(br.readLine());
-			A = Integer.parseInt(st.nextToken());
-			B = Integer.parseInt(st.nextToken());
 		}
-		br.close();
+		
 		System.out.println(sb);
 	}
 }
