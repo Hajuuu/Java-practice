788c9a272ab437b0178b9f2d4b794f6290fab161
Juyeon Ha <hpt0127@naver.com>
2023-02-28 23:49:17 +0900

    boj_9086

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -7,37 +7,14 @@ public class Algorithm {
 		
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int[] arr = new int[N];
+		int T = Integer.parseInt(br.readLine());
 		StringBuffer sb = new StringBuffer();
-		
-		for(int i = 0; i < N; i++) {
-			arr[i] = i + 1;
+		String str = "";
+		for(int i = 0; i < T; i++) {
+			str = br.readLine();
+			sb.append(str.charAt(0)).append(str.charAt(str.length()-1)).append("\n");
 		}
-		
-		for(int p = 0; p < M; p++) {
-			st = new StringTokenizer(br.readLine());
-			int i = Integer.parseInt(st.nextToken());
-			int j = Integer.parseInt(st.nextToken());
-			reverse(arr, i - 1, j);
-		}
-		
-		for(int i = 0; i < N; i++) {
-			sb.append(arr[i]).append(" ");
-		}
-		
 		System.out.println(sb);
 	}
 	
-	public static void reverse(int[] arr, int i, int j) {
-	
-		int[] copy = Arrays.copyOfRange(arr, i, j);
-		
-		int len = copy.length;
-		for(int p = 0; p < len; p++) {
-			arr[i+p] = copy[len-(p+1)]; 
-		}
-	}
 }
