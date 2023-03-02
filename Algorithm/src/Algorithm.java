fa7298956f25c52b93c5b950c39a140c2b1228c0
Juyeon Ha <hpt0127@naver.com>
2023-03-02 19:08:45 +0900

    boj_1152

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -7,24 +7,8 @@ public class Algorithm {
 		
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine().toUpperCase();
-		int len = str.length();
-		int[] arr = new int[26];
-		for(int i = 0; i < len; i++) {
-			arr[str.charAt(i) - 'A']++;
-		}
-		int max = 0;
-		char ch = ' ';
-		for(int i = 0; i < 26; i++) {
-			if(max < arr[i]) {
-				max = arr[i];
-				ch = (char) (i + 'A');
-			}
-			else if(max == arr[i]) 
-				ch = '?';
-		}
-		
-		System.out.println(ch);
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		System.out.println(st.countTokens());
 	}
 	
 }
