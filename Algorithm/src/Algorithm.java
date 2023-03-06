891a28c3dee4e6a01244c93688ded21c22604e36
Juyeon Ha <hpt0127@naver.com>
2023-03-06 14:18:46 +0900

    boj_10988

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -7,30 +7,18 @@ public class Algorithm {
 		
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuffer sb = new StringBuffer();
-		int N = Integer.parseInt(br.readLine());
-		int star = 2 * N - 1;
-		for(int i = 1; i <= N; i++) {
-			for(int j = N - i; j > 0; j--) {
-				sb.append(" ");
+		String word = br.readLine();
+		int len = word.length();
+		int q = 0;
+		for(int i = 0; i <= len / 2; i++) {
+			if(word.charAt(i) != word.charAt(len - i - 1)) {
+				q += 1;
 			}
-			for(int p = 1; p <= star - 2 * (N - i); p++) {
-				sb.append("*");
-			}
-			
-			sb.append("\n");
 		}
-		for(int i = 1; i < N; i++) {
-			for(int j = 0; j < i; j++) {
-				sb.append(" ");
-			}
-			for(int p = 1; p <= star - 2 * i; p++) {
-				sb.append("*");
-			}
-			
-			sb.append("\n");
-		}
-		System.out.println(sb);
+		if(q == 0)
+			System.out.println(1);
+		else
+			System.out.println(0);
 		
 	}
 	
