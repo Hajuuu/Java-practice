f31f0cdf99d0d56df1e2cf004a52a2f2aa6e452d
Juyeon Ha <hpt0127@naver.com>
2023-03-07 17:27:56 +0900

    boj_25206

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -5,39 +5,48 @@ import java.util.*;
 
 public class Algorithm {
 		
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
 	public static void main(String[] args) throws IOException {
-		int N = Integer.parseInt(br.readLine());
-		int group = 0;
-		for(int i = 0; i < N; i++) {
-			if(check() == true) {
-				group++;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		double total = 0;
+		double sum = 0;
+		for(int i = 0; i < 20; i++) {
+			String[] grade = br.readLine().split(" ");
+			if(!grade[2].equals("P")) {
+				total += Double.parseDouble(grade[1]);
 			}
-		}
-		System.out.println(group);
-	}
-	
-	public static boolean check() throws IOException {
-		boolean[] check = new boolean[26];
-		int prev = 0;
-		String str = br.readLine();
-		int len = str.length();
-		for(int i = 0; i < len; i++) {
-			int now = str.charAt(i);
 			
-			if(prev != now) {
-				if(check[now - 'a'] == false) {
-					check[now - 'a'] = true;
-					prev = now;
-				} else {
-					return false;
-				}
+			if(grade[2].equals("A+")) {
+				sum += 4.5 * Double.parseDouble(grade[1]);
 			}
+			else if(grade[2].equals("A0")) {
+				sum += 4.0 * Double.parseDouble(grade[1]);
+			}
+			else if(grade[2].equals("B+")) {
+				sum += 3.5 * Double.parseDouble(grade[1]);
+			}
+			else if(grade[2].equals("B0")) {
+				sum += 3.0 * Double.parseDouble(grade[1]);
+			}
+			else if(grade[2].equals("C+")) {
+				sum += 2.5 * Double.parseDouble(grade[1]);
+			}
+			else if(grade[2].equals("C0")) {
+				sum += 2.0 * Double.parseDouble(grade[1]);
+			}
+			else if(grade[2].equals("D+")) {
+				sum += 1.5 * Double.parseDouble(grade[1]);
+			}
+			else if(grade[2].equals("D0")) {
+				sum += 1.0 * Double.parseDouble(grade[1]);
+			}
+			else if(grade[2].equals("F")) {
+				sum += 0;
+			}
+			
 		}
-		
-		return true;
+		System.out.println(sum / total);
 	}
-	
 	
 	
 }
