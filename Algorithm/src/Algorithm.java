65bb4c2404a43c8faec728272276c19660d62705
Juyeon Ha <hpt0127@naver.com>
2023-03-07 16:42:07 +0900

    boj_1316

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -5,36 +5,39 @@ import java.util.*;
 
 public class Algorithm {
 		
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		int C = Integer.parseInt(br.readLine());
-		for(int i = 0; i < C; i++) {
-			int sum = 0;
-			double avg = 0;
-			st = new StringTokenizer(br.readLine(), " ");
-			int n = Integer.parseInt(st.nextToken());
-			int[] arr = new int[n];
-
-			for(int j = 0; j < n; j++) {
-				arr[j] = Integer.parseInt(st.nextToken());
-				sum += arr[j];
+		int N = Integer.parseInt(br.readLine());
+		int group = 0;
+		for(int i = 0; i < N; i++) {
+			if(check() == true) {
+				group++;
 			}
+		}
+		System.out.println(group);
+	}
+	
+	public static boolean check() throws IOException {
+		boolean[] check = new boolean[26];
+		int prev = 0;
+		String str = br.readLine();
+		int len = str.length();
+		for(int i = 0; i < len; i++) {
+			int now = str.charAt(i);
 			
-			avg = sum / n;
-			double a = 0;
-			for(int j = 0; j < n; j++) {
-				if(arr[j] > avg) {
-					a++;
+			if(prev != now) {
+				if(check[now - 'a'] == false) {
+					check[now - 'a'] = true;
+					prev = now;
+				} else {
+					return false;
 				}
 			}
-			sb.append(String.format("%.3f", a * 100 / n) + "%").append("\n");
 		}
 		
-		System.out.println(sb);
-		
+		return true;
 	}
+	
+	
 	
 }
