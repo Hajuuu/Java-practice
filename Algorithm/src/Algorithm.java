6540d03fdbcc052f2a8ec06c17a3707fb422e6f1
Juyeon Ha <hpt0127@naver.com>
2023-03-18 15:54:56 +0900

    boj_27866

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -8,28 +8,10 @@ public class Algorithm {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
 		int N = Integer.parseInt(br.readLine());
 		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int prime = 0;
-		while(st.hasMoreTokens()) {
-			int a = Integer.parseInt(st.nextToken());
-			int sum = 0;
-			if(a == 1)
-				continue;
-			
-			for(int i = 2; i < a; i++) {
-				if(a % i == 0) {
-					sum++;
-				}
-					
-			}
-			if(sum == 0) {
-				prime++;
-			}
-		}
-		
-		System.out.println(prime);
+		System.out.println(str.charAt(N - 1));
 
 	}
 }
