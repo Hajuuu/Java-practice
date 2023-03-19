c0782c51252e1ac10b6f83715f164ff31b7b8b70
Juyeon Ha <hpt0127@naver.com>
2023-03-19 16:01:19 +0900

    boj_2581

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -8,10 +8,44 @@ public class Algorithm {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
+
+		int M = Integer.parseInt(br.readLine());
 		int N = Integer.parseInt(br.readLine());
 		
-		System.out.println(str.charAt(N - 1));
+		int sum = 0;
+		int min = 10001;
+		for(int i = M; i <= N; i++) {
+			if(prime(i)) {
+				sum += i;
+				if(min > i) {
+					min = i;
+				}
+			}
+				
+		}
+		
+		if(sum == 0) {
+			System.out.println(-1);
+		} else {
+			System.out.println(sum + "\n" + min);
+		}
+		
 
+	}
+	
+	public static boolean prime(int a) {
+		boolean isPrime = true;
+		if(a == 1) {
+			isPrime = false;
+		}
+			
+		
+		for(int i = 2; i <= Math.sqrt(a); i++) {
+			if(a % i == 0) {
+				isPrime = false;
+			}
+		}
+		
+		return isPrime;
 	}
 }
