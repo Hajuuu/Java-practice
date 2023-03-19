3fffb37edc2cea781adeaa9f7b0470eaf395f0f4
Juyeon Ha <hpt0127@naver.com>
2023-03-19 22:38:49 +0900

    Ex13

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -2,28 +2,32 @@ package example;
 
 import java.util.*;
 
-class Parent {
-	void parentMethod() { }
-}
-
-class Child extends Parent {
-	@Override
-	@Deprecated
-	void parentMethod() { } // 조상 메서드의 이름을 잘못 적음.
-}
-
-@FunctionalInterface
-interface Testable {
-	void test(); // 추상메서드
-//	void check(); // 추상메서드
-}
-
 public class Practice2 {
 	
 	public static void main(String[] args) {
-		Child c = new Child();
-		c.parentMethod();
+		ThreadEx1_1 t1 = new ThreadEx1_1();
+		
+		Runnable r = new ThreadEx1_2();
+		Thread t2 = new Thread(r);
+		
+		t1.start();
+		t2.start();
 	}
 }
 
+class ThreadEx1_1 extends Thread { // Thread클래스를 상속해서 쓰레드를 구현
+	public void run() { // 쓰레드가 수행할 작업을 작성
+		for(int i = 0; i < 5; i++) {
+			System.out.println(this.getName()); // 조상인 Thread의 getName()을 호출
+		}
+	}
+}
 
+class ThreadEx1_2 implements Runnable { // Runnable인터페이스를 구현해서 쓰레드를 구현
+	public void run() { // 쓰레드가 수행할 작업을 작성
+		for(int i = 0; i < 5; i++) {
+			// Thread.currentThread() - 현재 실행중인 Thread를 반환한다.
+			System.out.println(Thread.currentThread().getName());
+		}
+	}
+}
