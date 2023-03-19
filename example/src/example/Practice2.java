0de28d027cf0a6133b78f4a70c0bf173214b30a1
Juyeon Ha <hpt0127@naver.com>
2023-03-20 00:23:26 +0900

    Ex13_5

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -7,13 +7,20 @@ import javax.swing.JOptionPane;
 public class Practice2 {
 	
 	public static void main(String[] args) {
-		String input = JOptionPane.showInputDialog("아무 값이나 입력하세요.");
-		System.out.println("입력하신 값은" + input + "입니다.");
+		ThreadEx5_1 th1 = new ThreadEx5_1();
+		th1.start();
 		
+		String input = JOptionPane.showInputDialog("아무 값이나 입력하세요.");
+		System.out.println("입력하신 값은 " + input + "입니다.");
+	}
+}
+
+class ThreadEx5_1 extends Thread {
+	public void run() {
 		for(int i = 10; i > 0; i--) {
 			System.out.println(i);
 			try {
-				Thread.sleep(1000);
+				sleep(1000);
 			} catch(Exception e) {}
 		}
 	}
