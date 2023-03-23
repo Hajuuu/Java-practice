9952d5a0d5af7290f3aea5169689386046c4afe9
Juyeon Ha <hpt0127@naver.com>
2023-03-23 14:19:39 +0900

    Ex13_10

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -7,26 +7,39 @@ import javax.swing.JOptionPane;
 public class Practice2 {
 	
 	public static void main(String[] args) {
-		ThreadEx9_1 th1 = new ThreadEx9_1();
-		th1.start();
+		RunImplEx10 r = new RunImplEx10();
+		Thread th1 = new Thread(r, "*");
+		Thread th2 = new Thread(r, "**");
+		Thread th3 = new Thread(r, "***");
 		
-		String input = JOptionPane.showInputDialog("아무 값이나 입력하세요.");
-		System.out.println("입력하신 값은 " + input + "입니다.");
-		th1.interrupt(); // interrupted상태가 true가 된다.
-		System.out.println("isInterrupted():" + th1.isInterrupted());
+		th1.start();
+		th2.start();
+		th3.start();
+		
+		try {
+			Thread.sleep(2000);
+			th1.suspend();
+			Thread.sleep(2000);
+			th2.suspend();
+			Thread.sleep(3000);
+			th1.resume();
+			Thread.sleep(3000);
+			th1.stop();
+			th2.stop();
+			Thread.sleep(2000);
+			th3.stop();
+		} catch(InterruptedException e) {}
 	}
 	
 }
 
-class ThreadEx9_1 extends Thread {
+class RunImplEx10 implements Runnable {
 	public void run() {
-		int i = 10;
-		
-		while(i!=0 && !isInterrupted()) {
-			System.out.println(i--);
-			for(long x = 0; x < 2500000000L; x++); //시간 지연
+		while(true) {
+			System.out.println(Thread.currentThread().getName());
+			try {
+				Thread.sleep(1000);
+			} catch(InterruptedException e) {}
 		}
-		
-		System.out.println("카운트가 종료되었습니다.");
 	}
 }
