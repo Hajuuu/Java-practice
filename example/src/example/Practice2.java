3d08d6f366e30033cb6a8ed751bcc2f0864f3c4c
Juyeon Ha <hpt0127@naver.com>
2023-03-23 14:33:28 +0900

    Ex13_10(1)

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -7,10 +7,9 @@ import javax.swing.JOptionPane;
 public class Practice2 {
 	
 	public static void main(String[] args) {
-		RunImplEx10 r = new RunImplEx10();
-		Thread th1 = new Thread(r, "*");
-		Thread th2 = new Thread(r, "**");
-		Thread th3 = new Thread(r, "***");
+		MyThread th1 = new MyThread("*");
+		MyThread th2 = new MyThread("**");
+		MyThread th3 = new MyThread("***");
 		
 		th1.start();
 		th2.start();
@@ -18,13 +17,13 @@ public class Practice2 {
 		
 		try {
 			Thread.sleep(2000);
-			th1.suspend();
+			th1.suspend(); // 쓰레드 th1을 잠시 중단시킨다.
 			Thread.sleep(2000);
 			th2.suspend();
 			Thread.sleep(3000);
-			th1.resume();
+			th1.resume(); // 쓰레드 th1이 다시 동작하도록 한다.
 			Thread.sleep(3000);
-			th1.stop();
+			th1.stop(); // 쓰레드 th1을 강제종료시킨다.
 			th2.stop();
 			Thread.sleep(2000);
 			th3.stop();
@@ -33,13 +32,40 @@ public class Practice2 {
 	
 }
 
-class RunImplEx10 implements Runnable {
+class MyThread implements Runnable {
+	volatile boolean suspended = false; // 쉽게 바뀌는 변수
+	volatile boolean stopped = false;
+	
+	Thread th;
+	
+	MyThread(String name) {
+		th = new Thread(this, name);
+	}
+	
+	void start() {
+		th.start();
+	}
+	
+	void stop() {
+		stopped = true;
+	}
+	
+	void suspend() {
+		suspended = true;
+	}
+	
+	void resume() {
+		suspended = false;
+	}
 	public void run() {
-		while(true) {
-			System.out.println(Thread.currentThread().getName());
-			try {
-				Thread.sleep(1000);
-			} catch(InterruptedException e) {}
+		while(!stopped) {
+			if(!suspended) {
+				System.out.println(Thread.currentThread().getName());
+				try {
+					Thread.sleep(1000);
+				} catch(InterruptedException e) {}
+			}
+			
 		}
 	}
 }
