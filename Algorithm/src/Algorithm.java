bc8e3e179d027922eaad65c29e36e47bd01f5c60
Juyeon Ha <hpt0127@naver.com>
2023-03-24 19:19:54 +0900

    boj_1085

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -4,48 +4,30 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Algorithm {
-		
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int M = Integer.parseInt(br.readLine());
-		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		int sum = 0;
-		int min = 10001;
-		for(int i = M; i <= N; i++) {
-			if(prime(i)) {
-				sum += i;
-				if(min > i) {
-					min = i;
-				}
-			}
-				
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		int h = Integer.parseInt(st.nextToken());
+		
+		int min = Integer.MAX_VALUE;
+		if(w - x < min) {
+			min = w - x;
+		}
+		if(h - y < min) {
+			min = h - y;
+		}
+		if(x < min) {
+			min = x;
+		}
+		if(y < min) {
+			min = y;
 		}
 		
-		if(sum == 0) {
-			System.out.println(-1);
-		} else {
-			System.out.println(sum + "\n" + min);
-		}
-		
-
-	}
-	
-	public static boolean prime(int a) {
-		boolean isPrime = true;
-		if(a == 1) {
-			isPrime = false;
-		}
-			
-		
-		for(int i = 2; i <= Math.sqrt(a); i++) {
-			if(a % i == 0) {
-				isPrime = false;
-			}
-		}
-		
-		return isPrime;
+		System.out.println(min);
 	}
 }
