9df585d7e90d4b686a824cbd035884f1b41b657a
Juyeon Ha <hpt0127@naver.com>
2023-03-25 14:29:11 +0900

    Ex14_1

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -4,102 +4,26 @@ import java.util.*;
 
 import javax.swing.JOptionPane;
 
-class Customer implements Runnable {
-	private Table table;
-	private String food;
-	
-	Customer(Table table, String food) {
-		this.table = table;
-		this.food = food;
-	}
-	
-	public void run() {
-		while(true) {
-			try { Thread.sleep(10); } catch(InterruptedException e) {}
-			String name = Thread.currentThread().getName();
-			
-			table.remove(food);
-			System.out.println(name + " ate a " + food);
-		}
-	}
-}
 
-class Cook implements Runnable {
-	private Table table;
-	
-	Cook(Table table) { this.table = table; }
-	
-	public void run() {
-		while(true) {
-			int idx = (int)(Math.random()*table.dishNum());
-			table.add(table.dishNames[idx]);
-			try { Thread.sleep(10);} catch(InterruptedException e) {}
-		}
-	}
-}
-
-class Table {
-	String[] dishNames = {"donut", "donut", "burger" };
-	final int MAX_FOOD = 6;
-	private ArrayList<String> dishes = new ArrayList<>();
-	public synchronized void add(String dish) {
-		while(dishes.size() >= MAX_FOOD) {
-			String name = Thread.currentThread().getName();
-			System.out.println(name + " is waiting.");
-			try {
-				wait(); // COOK쓰레드를 기다리게 한다.
-				Thread.sleep(500);
-			} catch(InterruptedException e) {}
-		}
-		dishes.add(dish);
-		notify();
-		System.out.println("Dishes:" + dishes.toString());
-	}
-	
-	public void remove(String dishName) {
-		synchronized(this) {
-			String name = Thread.currentThread().getName();
-			
-			while(dishes.size() == 0) {
-				System.out.println(name + " is waiting. ");
-				try {
-					wait(); // CUST쓰레드를 기다리게 한다.
-					Thread.sleep(500);
-				} catch(InterruptedException e) {}
-			}
-			
-			while(true) {
-				for(int i = 0; i < dishes.size(); i++) {
-					if(dishName.equals(dishes.get(i))) {
-						dishes.remove(i);
-						notify(); // 잠자고 있는 COOK을 깨우기 위함
-						return;
-					}
-				}
-				
-				try {
-					System.out.println(name + " is waiting.");
-					wait(); // 원하는 음식이 없는  CUST 쓰레드를 기다리게 한다.
-					Thread.sleep(500);
-				} catch(InterruptedException e) {}
-				
-			}
-		}
-	}
-	
-	public int dishNum() { return dishNames.length; }
-}
 public class Practice2 {
 	
 	public static void main(String[] args) throws Exception {
-		Table table = new Table(); // 여러 쓰레드가 공유하는 객체
+//		MyFunction f = new MyFunction() {
+//			public int max(int a, int b) { // 오버라이딩 - 접근제어자는 좁게 못바꾼다.
+//				return a > b ? a : b;
+//			}
+//		};
 		
-		new Thread(new Cook(table), "COOK").start();
-		new Thread(new Customer(table, "donut"), "CUST1").start();
-		new Thread(new Customer(table, "burger"), "CUST2").start();
+		//람다식(익명 객체)을 다루기 위한 참조변수의 타입은 함수형 인터페이스로 한다.
+		MyFunction f = (a, b) -> a > b ? a : b;
 		
-		Thread.sleep(2000);
-		System.exit(0);
+		int value = f.max(3, 5);
+		System.out.println("value=" + value);
 	}
 	
+}
+
+@FunctionalInterface
+interface MyFunction {
+	public abstract int max(int a, int b);
 }
