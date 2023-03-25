d015760b25b2ed2d5a05f1d88a87897d862e9987
Juyeon Ha <hpt0127@naver.com>
2023-03-25 16:38:34 +0900

    Ex14_2

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -1,44 +1,49 @@
 package example;
 
 import java.util.*;
+import java.util.function.*;
 
-import javax.swing.JOptionPane;
-
-@FunctionalInterface
-interface MyFunction {
-	void run();
-}
 
 public class Practice2 {
 	
-	static void execute(MyFunction f) { // 매개변수의 타입이 MyFunction인 메서드
-		f.run();
-	}
-	
-	static MyFunction getMyFunction() { // 반환 타입이 MyFunction인 메서드
-//		MyFunction f = () -> System.out.println("f3.run()");
-//		return f;
-		return () -> System.out.println("f3.run()");
-	}
-	
 	public static void main(String[] args) throws Exception {
-		// 람다식으로 MyFunction의 run()을 구현
-		MyFunction f1 = () -> System.out.println("f1.run()");
+		Supplier<Integer> s = () -> (int)(Math.random()*100)+1;
+		Consumer<Integer> c = i -> System.out.print(i + ", "); // 출력
+		Predicate<Integer> p = i -> i % 2 == 0; // 짝수인지 검사
+		Function<Integer, Integer> f = i -> i / 10 * 10;
 		
-		MyFunction f2 = new MyFunction() { // 익명클래스로 run()을 구현
-			public void run() { //public을 반드시 붙여야 함
-				System.out.println("f2.run()");
+		List<Integer> list = new ArrayList<>();
+		makeRandomList(s, list); // list를 랜덤값으로 채운다.
+		System.out.println(list);
+		printEvenNum(p, c, list); // 짝수를 출력
+		List<Integer> newList = doSomething(f, list);
+		System.out.println(newList);
+	}
+	
+	static <T> List<T> doSomething(Function<T, T> f, List<T> list) {
+		List<T> newList = new ArrayList<T>(list.size());
+		
+		for(T i : list) {
+			newList.add(f.apply(i));
+		}
+		
+		return newList;
+	}
+	
+	static <T> void printEvenNum(Predicate<T> p, Consumer<T> c, List<T> list) {
+		System.out.print("[");
+		for(T i : list) {
+			if(p.test(i)) { // 짝수인지 검사
+				c.accept(i); // i -> System.out.print(i+", "); 화면에 i 출력
 			}
-		};
-		
-		MyFunction f3 = getMyFunction();
-		
-		f1.run();
-		f2.run();
-		f3.run();
-		
-		execute(f1);
-		execute(() ->  System.out.println("run()"));
+		}
+		System.out.println("]");
+	}
+	
+	static <T> void makeRandomList(Supplier<T> s, List<T> list) {
+		for(int i = 0; i < 10; i++) {
+			list.add(s.get()); //Supplier로 부터 1~100의 난수를 받아서 list에 추가
+		}
 	}
 	
 }
