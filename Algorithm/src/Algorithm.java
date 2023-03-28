8a593e2a77daec506e5523432fe4382318834ab9
Juyeon Ha <hpt0127@naver.com>
2023-03-28 16:05:12 +0900

    boj_24266

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -1,23 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Algorithm {
 	
 	public static void main(String[] args) throws IOException {
-		// O(N^2)의 시간 복잡도
+		// O(N^3)의 시간 복잡도
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		long n = Integer.parseInt(br.readLine());
-		long i = n / 2;
-		if(n % 2 == 0) {			
-			System.out.println(n * (i - 1) + i);
-		} else {
-			System.out.println(n * i);
-		}
-		
-		System.out.println(2);
+		BigInteger n = new BigInteger(br.readLine());
+		System.out.println(n.multiply(n.multiply(n)));
+		System.out.println(3);
 		    
 	}
 }
