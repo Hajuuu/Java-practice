df12cd19e03589ba1dfe0bec25c5ff59a13b73d1
Juyeon Ha <hpt0127@naver.com>
2023-03-28 15:23:48 +0900

    boj_24263

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -6,9 +6,11 @@ import java.util.*;
 public class Algorithm {
 	
 	public static void main(String[] args) throws IOException {
+		// O(N)의 시간 복잡도
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		System.out.println(br.readLine());
 		System.out.println(1);
-		System.out.println(0);
 		
 		    
 	}
