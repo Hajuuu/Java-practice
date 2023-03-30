e882abdef26501d9688b9c367f32c0f96d3a705c
Juyeon Ha <hpt0127@naver.com>
2023-03-30 18:21:12 +0900

    boj_2798

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -7,11 +7,42 @@ import java.util.*;
 public class Algorithm {
 	
 	public static void main(String[] args) throws IOException {
-		// 시간 복잡도 O(N^3)
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		long n = Integer.parseInt(br.readLine());
-		System.out.println(n * (n - 1) * (n - 2) / 6);
-		System.out.println(3);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[] card = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < N; i++) {
+			card[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int max = 0;
+		int sum = 0;
+		
+		Loop1: 
+		for(int i = 0; i < N; i++) {
+			for(int j = i + 1; j < N; j++) {
+				for(int k = j + 1; k < N; k++) {
+					sum = card[i] + card[j] + card[k];
+					if(sum > M)
+						continue;
+					else if(sum < M) {
+						if(max < sum)
+							max = sum;
+					}
+					else {
+						max = sum;
+						break Loop1;
+					}
+				}
+			}
+		}
+		System.out.println(max);
 		    
 	}
 }
