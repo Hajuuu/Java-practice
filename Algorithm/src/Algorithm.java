4a750c8d32097939c0818139ffe160c0983f5b94
Juyeon Ha <hpt0127@naver.com>
2023-03-31 21:42:26 +0900

    boj_2839

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -8,23 +8,20 @@ public class Algorithm {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-		int d = Integer.parseInt(st.nextToken());
-		int e = Integer.parseInt(st.nextToken());
-		int f = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(br.readLine());
 		
-		for(int i = -999; i <= 999; i++) {
-			for(int j = -999; j <= 999; j++) {
-				if((a * i + b * j == c) && (d * i + e * j == f)) {
-					System.out.println(i + " " + j);
+		for(int i = 0; i <= 1000; i++) {
+			for(int j = 0; j <= 1000; j++) {
+				if((3 * i + 5 * j) == N) {
+					System.out.println(i + j);
 					return;
 				}
 			}
 		}
+		
+		System.out.println(-1);
+		
 	}
 	
 } 
