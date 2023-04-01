f768a25d06035497d712026d0918364a576c8abe
Juyeon Ha <hpt0127@naver.com>
2023-04-01 16:16:32 +0900

    boj_2751

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -8,21 +8,23 @@ public class Algorithm {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
 		
-		int N = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-		
+		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[N];
 		
-		st = new StringTokenizer(br.readLine(), " ");
 		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = Integer.parseInt(br.readLine());
 		}
 		
 		Arrays.sort(arr);
 		
-		System.out.println(arr[N - k]);
+		for(int i = 0; i < N; i++) {
+			sb.append(arr[i] + "\n");
+		}
+		
+		System.out.println(sb);
+		
 	}
 	
 } 
