b51940cbbbb9e242f96b2bef52ed2626ef56213b
Juyeon Ha <hpt0127@naver.com>
2023-04-02 21:07:27 +0900

    boj_10989

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -8,19 +8,21 @@ public class Algorithm {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+		StringBuffer sb = new StringBuffer();
 		
 		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
+		
+		int[] arr = new int[10001];
 		
 		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+			int n = Integer.parseInt(br.readLine());
+			arr[n]++;
 		}
 		
-		Arrays.sort(arr);
-		
-		for(int i = 0; i < N; i++) {
-			sb.append(arr[i] + "\n");
+		for(int i = 1; i < 10001; i++) {
+			for(int j = 1; j <= arr[i]; j++) {
+				sb.append(i).append("\n");
+			}
 		}
 		
 		System.out.println(sb);
