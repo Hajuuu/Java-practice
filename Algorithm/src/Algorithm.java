fca673a41e64478c119bf3fc5e38ef37d7b0deda
Juyeon Ha <hpt0127@naver.com>
2023-04-06 22:57:08 +0900

    boj_10828

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -1,41 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.Stack;
 
 public class Algorithm {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		int[] arr = new int[N];
-		int[] copyArr = new int[N];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < N; i++) {
-			arr[i] = copyArr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(copyArr);
-		
-		HashMap<Integer, Integer> hashMap = new HashMap<>();
-		
-		int j = 0;
-		for(int i : copyArr) {
-			if(!hashMap.containsKey(i)) {
-				hashMap.put(i, j);
-				j++;
-			}
-		}
-		
 		StringBuilder sb = new StringBuilder();
-		
-		for(int i : arr) {
-			sb.append(hashMap.get(i) + " ");
+		int N = Integer.parseInt(br.readLine());
+		Stack<Integer> stack = new Stack<>();
+		for(int i = 0; i < N; i++) {
+			String str = br.readLine();
+			if(str.charAt(0) == 'p' && str.charAt(1) == 'u') {
+				String[] st = str.split(" ");
+				stack.push(Integer.parseInt(st[1]));				
+			} else if(str.charAt(0) == 'p') {
+				if(!stack.empty()) {
+					sb.append(stack.pop() + "\n");
+				} else {
+					sb.append(-1 + "\n");
+				}
+				
+			} else if(str.charAt(0) == 's') {
+				sb.append(stack.size() + "\n");
+			} else if(str.charAt(0) == 'e') {
+				if(stack.empty()) {
+					sb.append(1 + "\n");
+				} else {
+					sb.append(0 + "\n");
+				}
+			} else if(str.charAt(0) == 't') {				
+				if(stack.empty()) {
+					sb.append(-1 + "\n");
+				} else {
+					sb.append(stack.peek() + "\n");
+				}
+			}
 		}
 		
 		System.out.println(sb);
