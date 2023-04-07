8cbe055f69b8be44876dd31e7c0f47d3041a7235
Juyeon Ha <hpt0127@naver.com>
2023-04-07 22:34:41 +0900

    boj_9093

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -1,44 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Algorithm {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		StringBuilder sd = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
-		Stack<Integer> stack = new Stack<>();
+		
 		for(int i = 0; i < N; i++) {
-			String str = br.readLine();
-			if(str.charAt(0) == 'p' && str.charAt(1) == 'u') {
-				String[] st = str.split(" ");
-				stack.push(Integer.parseInt(st[1]));				
-			} else if(str.charAt(0) == 'p') {
-				if(!stack.empty()) {
-					sb.append(stack.pop() + "\n");
-				} else {
-					sb.append(-1 + "\n");
-				}
-				
-			} else if(str.charAt(0) == 's') {
-				sb.append(stack.size() + "\n");
-			} else if(str.charAt(0) == 'e') {
-				if(stack.empty()) {
-					sb.append(1 + "\n");
-				} else {
-					sb.append(0 + "\n");
-				}
-			} else if(str.charAt(0) == 't') {				
-				if(stack.empty()) {
-					sb.append(-1 + "\n");
-				} else {
-					sb.append(stack.peek() + "\n");
-				}
+			st = new StringTokenizer(br.readLine(), " ");
+			while(st.hasMoreTokens()) {
+				StringBuffer sb = new StringBuffer(st.nextToken());
+				String str = sb.reverse().toString();
+				sd.append(str + " ");
 			}
+			sd.append("\n");
+			
 		}
 		
-		System.out.println(sb);
+		System.out.println(sd.toString());
+		
 	}
 } 
