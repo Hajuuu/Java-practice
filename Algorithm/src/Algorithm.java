ecc79b9f456f0e9da212931a7b7c670206fe0a26
Juyeon Ha <hpt0127@naver.com>
2023-04-09 14:14:41 +0900

    boj_9012

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -1,28 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Stack;
 
 public class Algorithm {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sd = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
+		StringBuffer sb = new StringBuffer();
+		Stack<String> stack = new Stack<>();
+		// '(' 일때 push ')' 일때 pop
 		
 		for(int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			while(st.hasMoreTokens()) {
-				StringBuffer sb = new StringBuffer(st.nextToken());
-				String str = sb.reverse().toString();
-				sd.append(str + " ");
+			String[] arr = br.readLine().split("");
+			stack.clear();
+			for(String s : arr) {
+				
+				if(s.equals("(")) {
+					stack.push("(");
+				}
+				else {
+					if(!stack.empty() && stack.peek() != ")") {
+						stack.pop();
+					} else {
+						stack.push(")");
+					}
+				}
+				
 			}
-			sd.append("\n");
-			
+			if(stack.empty()) {
+				sb.append("YES\n");
+			} else {
+				sb.append("NO\n");
+			}
 		}
-		
-		System.out.println(sd.toString());
-		
+			
+		System.out.println(sb);
 	}
 } 
