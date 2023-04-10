43059f5604e380ecad580c867daf0ce4049ef413
Juyeon Ha <hpt0127@naver.com>
2023-04-10 15:22:29 +0900

    boj_1158

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -3,54 +3,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Algorithm {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuffer sb = new StringBuffer();
-		int N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
 		
 		Queue<Integer> queue = new LinkedList<>();
-		int tail = 0;
-		for(int i = 0; i < N; i++) {
-			String str = br.readLine();
-			if(str.charAt(0) == 'p'  && str.charAt(1) == 'u') {
-				String[] st = str.split(" ");
-				queue.add(Integer.parseInt(st[1]));
-				tail = Integer.parseInt(st[1]);
-			} else if(str.charAt(0) == 'p') {
-				if(queue.isEmpty()) {
-					sb.append(-1 + "\n");
-				} else {
-					sb.append(queue.poll() + "\n");
-				}
-			
-			} else if(str.charAt(0) == 's') {
-				sb.append(queue.size() + "\n");
-				
-			} else if(str.charAt(0) == 'e') {
-				if(queue.isEmpty()) {
-					sb.append(1 + "\n");
-				} else {
-					sb.append(0 + "\n");
-				}
-			} else if(str.charAt(0) == 'f') {
-				if(queue.isEmpty()) {
-					sb.append(-1 + "\n");
-				} else {
-					sb.append(queue.peek() + "\n");
-				}
-				
-			} else {
-				if(queue.isEmpty()) {
-					sb.append(-1 + "\n");
-				} else {
-					sb.append(tail + "\n");
-				}
+		String str = "";
+		for(int i = 1; i <= N; i++) {
+			queue.add(i);
+		}
+		
+		sb.append("<");
+		
+		while(queue.size() > 1) {
+			for(int i = 0; i < K - 1; i++) {
+				queue.add(queue.poll());
 			}
+			sb.append(queue.poll());
+			sb.append(", ");
 			
 		}
+		sb.append(queue.poll());
+		sb.append(">");
 		
 		System.out.println(sb);
 	}
