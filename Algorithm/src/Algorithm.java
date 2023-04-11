83e2559156d07e522fbcc1d1d290a2826b738876
Juyeon Ha <hpt0127@naver.com>
2023-04-11 13:13:24 +0900

    boj_17413

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -1,67 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.Stack;
 
 public class Algorithm {
 	
 	public static void main(String[] args) throws IOException {
+		//스택에 넣다가 괄호 나오면 sb.append, pop
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuffer sb = new StringBuffer();
-		int N = Integer.parseInt(br.readLine());
 		
-		Deque<Integer> deque = new LinkedList<>();
-		
-		for(int i = 0; i < N; i++) {
-			String str = br.readLine();
-			if(str.charAt(0) == 'p' && str.charAt(1) == 'u') {
-				String[] arr = str.split(" ");
-				if(str.charAt(5) == 'b') {
-					deque.offerLast(Integer.parseInt(arr[1]));
-				} else {
-					deque.offerFirst(Integer.parseInt(arr[1]));
+		Stack<Character> stack = new Stack<>();
+		String str = br.readLine();
+		int len = str.length();
+		for(int i = 0; i < len; i++) {
+			if(str.charAt(i) == '<') {
+				while(!stack.empty()) {
+					sb.append(stack.pop());
+				}
+				while(str.charAt(i) != '>') {
+					sb.append(str.charAt(i));
+					i++;
+				}
+				if(str.charAt(i) == '>') {
+					sb.append(">");
 				}
 			}
-			else if(str.charAt(0) == 'p') {
-				if(deque.isEmpty()) {
-					sb.append(-1 + "\n");
-					continue;
+			else if(str.charAt(i) == ' ') {
+				while(!stack.empty()) {
+					sb.append(stack.pop());
 				}
-				if(str.charAt(4) == 'f') {
-					sb.append(deque.peek() + "\n");
-					deque.poll();
-				} else {
-					sb.append(deque.peekLast() + "\n");
-					deque.pollLast();
-				}
-			}
-			else if(str.charAt(0) == 's') {
-				sb.append(deque.size() + "\n");
-			}
-			else if(str.charAt(0) == 'e') {
-				if(deque.isEmpty()) {
-					sb.append(1 + "\n");
-				} else {
-					sb.append(0 + "\n");
-				}
-			}
-			else if(str.charAt(0) == 'f') {
-				if(deque.isEmpty()) {
-					sb.append(-1 + "\n");
-				} else {
-					sb.append(deque.peek() + "\n");
-				}
+				sb.append(" ");
 			}
 			else {
-				if(deque.isEmpty()) {
-					sb.append(-1 + "\n");
-				} else {
-					sb.append(deque.peekLast() + "\n");
-				}
+				stack.push(str.charAt(i));
 			}
-			
-			
+		}
+		
+		while(!stack.empty()) {
+			sb.append(stack.pop());
 		}
 		System.out.println(sb);
 		
