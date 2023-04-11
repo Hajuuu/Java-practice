0c23265d60f607468055a172c04cc0e788a81f93
Juyeon Ha <hpt0127@naver.com>
2023-04-11 12:35:38 +0900

    boj_10866

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -1,39 +1,70 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Algorithm {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuffer sb = new StringBuffer();
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(br.readLine());
 		
-		Queue<Integer> queue = new LinkedList<>();
-		String str = "";
-		for(int i = 1; i <= N; i++) {
-			queue.add(i);
-		}
+		Deque<Integer> deque = new LinkedList<>();
 		
-		sb.append("<");
-		
-		while(queue.size() > 1) {
-			for(int i = 0; i < K - 1; i++) {
-				queue.add(queue.poll());
+		for(int i = 0; i < N; i++) {
+			String str = br.readLine();
+			if(str.charAt(0) == 'p' && str.charAt(1) == 'u') {
+				String[] arr = str.split(" ");
+				if(str.charAt(5) == 'b') {
+					deque.offerLast(Integer.parseInt(arr[1]));
+				} else {
+					deque.offerFirst(Integer.parseInt(arr[1]));
+				}
 			}
-			sb.append(queue.poll());
-			sb.append(", ");
+			else if(str.charAt(0) == 'p') {
+				if(deque.isEmpty()) {
+					sb.append(-1 + "\n");
+					continue;
+				}
+				if(str.charAt(4) == 'f') {
+					sb.append(deque.peek() + "\n");
+					deque.poll();
+				} else {
+					sb.append(deque.peekLast() + "\n");
+					deque.pollLast();
+				}
+			}
+			else if(str.charAt(0) == 's') {
+				sb.append(deque.size() + "\n");
+			}
+			else if(str.charAt(0) == 'e') {
+				if(deque.isEmpty()) {
+					sb.append(1 + "\n");
+				} else {
+					sb.append(0 + "\n");
+				}
+			}
+			else if(str.charAt(0) == 'f') {
+				if(deque.isEmpty()) {
+					sb.append(-1 + "\n");
+				} else {
+					sb.append(deque.peek() + "\n");
+				}
+			}
+			else {
+				if(deque.isEmpty()) {
+					sb.append(-1 + "\n");
+				} else {
+					sb.append(deque.peekLast() + "\n");
+				}
+			}
+			
 			
 		}
-		sb.append(queue.poll());
-		sb.append(">");
-		
 		System.out.println(sb);
+		
 	}
 	
 	
