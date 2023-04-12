9ee6d9d216b293bdb532e64bd0bd1026e31ae310
Juyeon Ha <hpt0127@naver.com>
2023-04-13 01:14:04 +0900

    boj_1935

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -11,37 +11,46 @@ public class Algorithm {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		
-		Stack<Integer> stack1 = new Stack<>();
-		int[] arr = new int[N];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < N; i++) {
-			int n = Integer.parseInt(st.nextToken());
-			stack1.push(n);
-			arr[i] = n;
-		}
-		int max = Arrays.stream(arr).max().getAsInt();
-		int[] F = new int[max + 1];
-		while(!stack1.empty()) {
-			F[stack1.pop()]++;
-		}
+		String str = br.readLine();
+		double[] arr = new double[N];
 		
 		for(int i = 0; i < N; i++) {
-			while(!stack1.empty() && F[arr[stack1.peek()]] < F[arr[i]]) {
-				arr[stack1.pop()] = arr[i]; 
+			arr[i] = Double.parseDouble(br.readLine());
+		}
+		
+		Stack<Double> stack = new Stack<>();
+		
+		int len = str.length();
+		
+		for(int i = 0; i < len; i++) {
+			if(str.charAt(i) >= 65 && str.charAt(i) <= 90) {
+				stack.push((arr[str.charAt(i) - 'A']));
+			} else {
+				if(str.charAt(i) == '*') {
+					double n = stack.pop() * stack.pop();
+					stack.push(n);
+				}
+				else if(str.charAt(i) == '/') {
+					double a = stack.pop();
+					double b = stack.pop();
+					double n = b / a;
+					stack.push(n);
+				}else if(str.charAt(i) == '+') {
+					double n = stack.pop() + stack.pop();
+					stack.push(n);
+				}
+				else if(str.charAt(i) == '-') {
+					double a = stack.pop();
+					double b = stack.pop();
+					stack.push(b - a);
+				}
+				
 			}
-			stack1.push(i);
+			
+			
 		}
 		
-		while(!stack1.empty()) {
-			arr[stack1.pop()] = -1;
-		}
-		StringBuffer sb = new StringBuffer();
-		for(int i = 0; i < N; i++) {
-			sb.append(arr[i] + " ");
-		}
-		
-		System.out.println(sb);
-		
+		System.out.printf("%.2f", stack.pop());
 	}
 	
 	
