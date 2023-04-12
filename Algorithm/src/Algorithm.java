bf9f5be5fb46b8e3f33262dd6227e9e2fd034438
Juyeon Ha <hpt0127@naver.com>
2023-04-12 16:32:12 +0900

    boj_17299

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -1,32 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Algorithm {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
-		int len = str.length();
+		int N = Integer.parseInt(br.readLine());
 		
-		int stick = 0;
-		Stack<Character> stack = new Stack<>();
-		for(int i = 0; i < len; i++) {
-			if(str.charAt(i) == '(') {
-				stack.push('(');
-			} else if(str.charAt(i) == ')') {
-				if(str.charAt(i - 1) == '(') {
-					stack.pop();
-					stick += stack.size();
-				} else {			
-					stack.pop();
-					stick += 1;
-				}
-			}
+		Stack<Integer> stack1 = new Stack<>();
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			int n = Integer.parseInt(st.nextToken());
+			stack1.push(n);
+			arr[i] = n;
+		}
+		int max = Arrays.stream(arr).max().getAsInt();
+		int[] F = new int[max + 1];
+		while(!stack1.empty()) {
+			F[stack1.pop()]++;
 		}
 		
-		System.out.println(stick);
+		for(int i = 0; i < N; i++) {
+			while(!stack1.empty() && F[arr[stack1.peek()]] < F[arr[i]]) {
+				arr[stack1.pop()] = arr[i]; 
+			}
+			stack1.push(i);
+		}
+		
+		while(!stack1.empty()) {
+			arr[stack1.pop()] = -1;
+		}
+		StringBuffer sb = new StringBuffer();
+		for(int i = 0; i < N; i++) {
+			sb.append(arr[i] + " ");
+		}
+		
+		System.out.println(sb);
 		
 	}
 	
