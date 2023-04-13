b508353da8df80f850cc416bb2cc76c48783c2bd
Juyeon Ha <hpt0127@naver.com>
2023-04-13 20:10:09 +0900

    상속 및 포함

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -3,37 +3,33 @@ package example;
 import java.util.*;
 import java.util.function.*;
 
+class Point {
+	int x;
+	int y;
+}
+
+//class Circle extends Point { // 상속
+//	int r; 
+//}
+
+class Circle { // 포함
+	Point p = new Point();
+	int r;
+}
+
 
 public class Practice2 {
 	
 	public static void main(String[] args) throws Exception {
-		MyMath mm = new MyMath(3, 4);
-		long result = MyMath.add(3L, 4L);
-		int result2 = mm.add(3,4);
-		long result3 = mm.add();
-		System.out.println(result + " " + result2 + " " + result3);
+		Circle c = new Circle();
+		c.p.x = 1;
+		c.p.y = 2;
+		c.r = 3;
+		System.out.println("c.x=" + c.p.x);
+		System.out.println("c.y=" + c.p.y);
+		System.out.println("c.r=" + c.r);
+		
 	}
 	
 }
 
-class MyMath {
-	long a, b;
-	int sum = 5;
-	
-	MyMath(int a, int b) {
-		this.a = a;
-		this.b = b;
-	}
-	
-	long add() {
-		return this.a + this.b;
-	}
-	
-	static long add(long a, long b) {
-		return a + b;
-	}
-	
-	int add(int a, int b) {
-		return sum + a + b;
-	}
-}
