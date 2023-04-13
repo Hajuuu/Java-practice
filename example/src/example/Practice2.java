a90a5a8cbf9fcdb7defe717c3d08ea50d7f80490
Juyeon Ha <hpt0127@naver.com>
2023-04-13 20:34:18 +0900

    OverrideTest

--- example/src/example/Practice2.java
+++ example/src/example/Practice2.java
@@ -6,28 +6,23 @@ import java.util.function.*;
 class Point {
 	int x;
 	int y;
-}
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 
-//class Circle extends Point { // 상속
-//	int r; 
-//}
-
-class Circle { // 포함
-	Point p = new Point();
-	int r;
+	public String toString() {
+		return "x:" + x +", y:" + y;
+	}
 }
 
 
 public class Practice2 {
 	
 	public static void main(String[] args) throws Exception {
-		Circle c = new Circle();
-		c.p.x = 1;
-		c.p.y = 2;
-		c.r = 3;
-		System.out.println("c.x=" + c.p.x);
-		System.out.println("c.y=" + c.p.y);
-		System.out.println("c.r=" + c.r);
+		Point p = new Point(3,5);
+		System.out.println(p);
 		
 	}
 	
