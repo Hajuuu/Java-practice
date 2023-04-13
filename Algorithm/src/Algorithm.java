b8ec417e5efd2301d07234247363d03079b4713a
Juyeon Ha <hpt0127@naver.com>
2023-04-13 17:42:37 +0900

    boj_11656

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -10,14 +10,24 @@ public class Algorithm {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		String[] str = br.readLine().split(" ");
+		String S = br.readLine();
 		
-		String AB = str[0] + str[1];
-		String CD = str[2] + str[3];
+		int len = S.length();
 		
-		long sum = Long.parseLong(AB) + Long.parseLong(CD);
+		String[] arr = new String[len];
 		
-		System.out.println(sum);
+		for(int i = 0; i < len; i++) {
+			arr[i] = S.substring(i, len);
+		}
+		
+		Arrays.sort(arr);
+		
+		StringBuffer sb = new StringBuffer();
+		for(int i = 0; i < len; i++) {
+			sb.append(arr[i] + "\n");
+		}
+		
+		System.out.println(sb);
  	}
 	
 	
