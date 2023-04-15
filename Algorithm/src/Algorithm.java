f15bc890675d42df35693c8d5908d63ba4835e2a
Juyeon Ha <hpt0127@naver.com>
2023-04-15 23:52:37 +0900

    boj_10872

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -9,45 +9,20 @@ public class Algorithm {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		boolean[] prime = prime(1000000);
 		
-		int n = Integer.parseInt(br.readLine());
-		while(n != 0) {		
-			boolean gold = false;
-			for(int i = 2; i <= n / 2; i++) {
-				if(!prime[i] && !prime[n - i]) {
-					System.out.println(n + " = " + i + " + " + (n - i));
-					gold = true;
-					break;
-				}
-			}
-			
-			if(!gold) {
-				System.out.println("Goldbach's conjecture is wrong.");
-			}
+		int N = Integer.parseInt(br.readLine());
 		
-			n = Integer.parseInt(br.readLine());
-		}
+		int fac = factorial(1, N);
+		
+		System.out.println(fac);
  	}
 	
-	public static boolean[] prime(int n) {
-		boolean[] prime = new boolean[n + 1];
-		
-		prime[0] = true;
-		prime[1] = true;
-		
-		for(int i = 2; i <= Math.sqrt(n); i++) {
-			if(prime[i] == true) {
-				continue;
-			}
-			
-			for(int j = i * i; j <= n; j += i) {
-				prime[j] = true;
-			}
+	public static int factorial(int a, int b) {
+		if(b == 0) {
+			return a;
+		} else {
+			return factorial(a * b, b - 1);
 		}
-		
-		return prime;
 	}
-	
 	
 } 
