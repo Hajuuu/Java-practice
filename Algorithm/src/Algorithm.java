7948c15296cce001357d78a9d0a7cdfcdfa49847
Juyeon Ha <hpt0127@naver.com>
2023-04-15 23:10:03 +0900

    boj_6588

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -9,42 +9,44 @@ public class Algorithm {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		boolean[] prime = prime(1000000);
 		
-		int M = Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());
-		
-		boolean[] prime = prime(N);
-		StringBuffer sb = new StringBuffer();
-		for(int i = M; i <= N; i++) {
-			if(prime[i] == false) {
-				sb.append(i + "\n");
+		int n = Integer.parseInt(br.readLine());
+		while(n != 0) {		
+			boolean gold = false;
+			for(int i = 2; i <= n / 2; i++) {
+				if(!prime[i] && !prime[n - i]) {
+					System.out.println(n + " = " + i + " + " + (n - i));
+					gold = true;
+					break;
+				}
 			}
+			
+			if(!gold) {
+				System.out.println("Goldbach's conjecture is wrong.");
+			}
+		
+			n = Integer.parseInt(br.readLine());
 		}
-		
-		System.out.println(sb);
-		
  	}
 	
-	public static boolean[] prime(int N) {
-		boolean[] prime = new boolean[N + 1];
+	public static boolean[] prime(int n) {
+		boolean[] prime = new boolean[n + 1];
 		
 		prime[0] = true;
 		prime[1] = true;
 		
-		for(int i = 2; i <= Math.sqrt(N); i++) {
+		for(int i = 2; i <= Math.sqrt(n); i++) {
 			if(prime[i] == true) {
 				continue;
 			}
 			
-			for(int j = i * i; j <= N; j += i) {
+			for(int j = i * i; j <= n; j += i) {
 				prime[j] = true;
 			}
-			
 		}
 		
 		return prime;
-		
 	}
 	
 	
