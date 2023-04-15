e0f4a11004a814747f3bfdd5c0aeaaeff51e2d9e
Juyeon Ha <hpt0127@naver.com>
2023-04-15 14:41:11 +0900

    boj_1934

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -9,24 +9,30 @@ public class Algorithm {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		StringBuffer sb = new StringBuffer();
+		for(int i = 0; i < T; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			int max = Math.max(A, B);
+			int min = Math.min(A, B);
+			
+			int gcd = GCD(max, min);
+			int lcm = A * B / gcd;
+			
+			sb.append(lcm + "\n");
+		}
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		
-		int max = Math.max(a, b);
-		int min = Math.min(a, b);
-		int gcd = GCD(max, min);
-		int lcm = a * b / gcd;
-		System.out.println(gcd);
-		System.out.println(lcm);
-		
+		System.out.println(sb);
  	}
 	
 	public static int GCD(int a, int b) {
-		if(b == 0)
+		if(b == 0) {
 			return a;
+		}
 		else {
 			return GCD(b, a % b);
 		}
