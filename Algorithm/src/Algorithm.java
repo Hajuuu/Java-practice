c36ce2a53b53869b317f5ab62504042ed2b9f687
Juyeon Ha <hpt0127@naver.com>
2023-04-16 17:06:56 +0900

    boj_2004

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -10,14 +10,36 @@ public class Algorithm {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = Integer.parseInt(br.readLine());
-
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int count1 = zero1(n) - zero1(n - m) - zero1(m);
+		int count2 = zero2(n) - zero2(n - m) - zero2(m);
+		
+		System.out.println(Math.min(count1, count2));
+	}
+	
+	public static int zero1(int num) {
 		int count = 0;
-		while(N >= 5) {
-			count += N / 5;
-			N /= 5;
+		
+		while(num >= 5) {
+			count += num / 5;
+			num /= 5;
 		}
 		
-		System.out.println(count);
- 	}
+		return count;
+	}
+	
+	public static int zero2(int num) {
+		int count = 0;
+		
+		while(num >= 2) {
+			count += num / 2;
+			num /= 2;
+		}
+		
+		return count;
+	}
 } 
