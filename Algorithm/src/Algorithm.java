bd71e118552dbe23a6804a7e3a1e76c5622b4733
Juyeon Ha <hpt0127@naver.com>
2023-04-22 21:02:51 +0900

    boj_6064

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -8,178 +8,41 @@ public class Algorithm {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		int[][] arr = new int[N][M];
-		
-		for(int i = 0; i < N; i++) {
+		StringTokenizer st;
+		StringBuffer sb = new StringBuffer();
+		for(int i = 0; i < T; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < M; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+			
+			int M = Integer.parseInt(st.nextToken());
+			int N = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken()) - 1;
+			int y = Integer.parseInt(st.nextToken()) - 1;
+			
+			int year = x;
+			int last = M * N / gcd(M, N);
+			
+			while(year <= last) {
+				if((year % N == y)) {
+					sb.append(year + 1 + "\n");
+					break;
+				}
+				year += M;
+			}
+			
+			if(year > last) {
+				sb.append(-1 + "\n");
+				continue;
 			}
 		}
 		
-		int max = Integer.MIN_VALUE;
-		int sum = 0;
-		// case 1-1
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < M - 3; j++) {
-				sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i][j + 3];
-				max = Math.max(sum, max);
-			}
-		}
-		
-		//case 1-2
-		for(int i = 0; i < M; i++) {
-			for(int j = 0; j < N - 3; j++) {
-				sum = arr[j][i] + arr[j + 1][i] + arr[j + 2][i] + arr[j + 3][i];
-				max = Math.max(sum, max);
-			}
-		}
-		
-		//case 2
-		for(int i = 0; i < N - 1; i++) {
-			for(int j = 0; j < M - 1; j++) {
-				sum = arr[i][j] + arr[i + 1][j] + arr[i][j + 1] + arr[i + 1][j + 1];
-				max = Math.max(sum, max);
-			}
-		}
-		
-		//case 3-1
-		for(int i = 0; i < N - 2; i++) {
-			for(int j = 0; j < M - 1; j++) {
-				sum = arr[i][j] + arr[i + 1][j] + arr[i + 2][j] + arr[i + 2][j + 1];
-				max = Math.max(sum, max);
-			}
-		}
-		
-		//case 3 - 2
-		for(int i = 0; i < N - 2; i++) {
-			for(int j = 0; j < M - 1; j++) {
-				sum = arr[i][j + 1] + arr[i + 1][j + 1] + arr[i + 2][j + 1] + arr[i + 2][j];
-				max = Math.max(sum, max);
-			}
-		}
-		
-		// case 3 - 3
-		for(int i = 0; i < N - 1; i++) {
-			for(int j = 0; j < M - 2; j++) {
-				sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j];
-				max = Math.max(sum, max);
-			}
-		}
-		
-		// case 3 - 4
-		for(int i = 0; i < N - 1; i++) {
-			for(int j = 0; j < M - 2; j++) {
-				sum = arr[i + 1][j] + arr[i + 1][j + 1] + arr[i + 1][j + 2] + arr[i][j + 2];
-				max = Math.max(sum, max);
-			}
-		}
-		
-		
-		// case 3 - 5
-		for(int i = 0; i < N - 2; i++) {
-			for(int j = 0; j < M - 1; j++) {
-				sum = arr[i][j] + arr[i][j + 1] + arr[i + 1][j + 1] + arr[i + 2][j + 1];
-				max = Math.max(sum, max);
-			}
-		}
-		
-		// case 3 - 6
-		for(int i = 0; i < N - 2; i++) {
-			for(int j = 0; j < M - 1; j++) {
-				sum = arr[i][j] + arr[i][j + 1] + arr[i + 1][j] + arr[i + 2][j];
-				max = Math.max(sum, max);
-			}
-		}
-		
-		// case 3 - 7
-		for(int i = 0; i < N - 1; i++) {
-			for(int j = 0; j < M - 2; j++) {
-				sum = arr[i][j] + arr[i + 1][j] + arr[i + 1][j + 1] + arr[i + 1][j + 2];
-				max = Math.max(sum, max);
- 			}
-		}
-		
-		// case 3 - 8
-		for(int i = 0; i < N - 1; i++) {
-			for(int j = 0; j < M - 2; j++) {
-				sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 2];
-				max = Math.max(sum, max);
-			}
-		}
-		// case 4 - 1
-		for(int i = 0; i < N - 2; i++) {
-			for(int j = 0; j < M - 1; j++) {
-				sum = arr[i][j + 1] + arr[i + 1][j] + arr[i + 1][j + 1] + arr[i + 2][j];
-				max = Math.max(sum, max);
-			}
-		}
-		
-		
-		// case 4 - 2
-		for(int i = 0; i < N - 2; i++) {
-			for(int j = 0; j < M - 1; j++) {
-				sum = arr[i][j] + arr[i + 1][j] + arr[i + 1][j + 1] + arr[i + 2][j + 1];
-				max = Math.max(sum, max);
-			}
-		}
-		
-		// case 4 - 3
-		for(int i = 0; i < N - 1; i++) {
-			for(int j = 0; j < M - 2; j++) {
-				sum = arr[i][j] + arr[i][j + 1] + arr[i + 1][j + 1] + arr[i + 1][j + 2];
-				max = Math.max(sum, max);
-			}
-		}
-		
-		// case 4 - 4
-		for(int i = 0; i < N - 1; i++) {
-			for(int j = 0; j < M - 2; j++) {
-				sum = arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j] + arr[i + 1][j + 1];
-				max = Math.max(sum, max);
-			}
-		}
-		
-		
-		// case 5 - 1
-		for(int i = 0; i < N - 1; i++) {
-			for(int j = 0; j < M - 2; j++) {
-				sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1];
-				max = Math.max(sum, max);
-			}
-		}
-		
-		// case 5 - 2
-		for(int i = 0; i < N - 1; i++) {
-			for(int j = 0; j < M - 2; j++) {
-				sum = arr[i + 1][j] + arr[i + 1][j + 1] + arr[i + 1][j + 2] + arr[i][j + 1];
-				max = Math.max(sum, max);
-			}
-		}
-		
-		// case 5 - 3
-		for(int i = 0; i < N - 2; i++) {
-			for(int j = 0; j < M - 1; j++) {
-				sum = arr[i][j] + arr[i + 1][j] + arr[i + 2][j] + arr[i + 1][j + 1];
-				max = Math.max(sum, max);
-			}
-		}
-		
-		// case 5 - 4
-		for(int i = 0; i < N - 2; i++) {
-			for(int j = 0; j < M - 1; j++) {
-				sum = arr[i][j + 1] + arr[i + 1][j + 1] + arr[i + 2][j + 1] + arr[i + 1][j];
-				max = Math.max(sum, max);
-			}
-		}
-		
-		
-		System.out.println(max);
+		System.out.println(sb);
+	}
+	
+	public static int gcd(int M, int N) {
+		if(N == 0)
+			return M;
+		return gcd(N, M % N);
 	}
 } 
