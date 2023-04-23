9f08a7c14da457c3512b36aa6a89e6b308d1ddbd
Juyeon Ha <hpt0127@naver.com>
2023-04-23 21:04:47 +0900

    boj_15654

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Algorithm {
@@ -10,6 +9,8 @@ public class Algorithm {
 	static int N;
 	static int M;
 	static int[] arr;
+	static int[] darr;
+	static boolean[] isUsed;
 	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,27 +20,44 @@ public class Algorithm {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		arr = new int[M];
+		st = new StringTokenizer(br.readLine());
+		arr = new int[N];
+		darr = new int[M];
 		
-		dfs(1, 0);
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		isUsed = new boolean[arr[N - 1] + 1];
+		
+		dfs(0);
+		
 		
 		System.out.println(sb);
 		
-		
 	}
 	
-	public static void dfs(int at, int depth) {
+	public static void dfs(int depth) {
 		if(depth == M) {
 			for(int i = 0; i < M; i++) {
-				sb.append(arr[i] + " ");
+				sb.append(darr[i] + " ");			
 			}
 			sb.append("\n");
 			return;
 		}
 		
-		for(int i = at; i <= N; i++) {
-			arr[depth] = i;
-			dfs(i, depth + 1);
+		for(int i = 0; i < N; i++) {
+			if(!isUsed[arr[i]]) {
+				isUsed[arr[i]] = true;
+				darr[depth] = arr[i];
+				dfs(depth + 1);
+				
+				isUsed[arr[i]] = false;
+			}
+			
+			
 		}
 	}
 } 
