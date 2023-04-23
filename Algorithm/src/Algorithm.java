6ee01e97e8a3c253b52a21aaa4a2a1b266af63f3
Juyeon Ha <hpt0127@naver.com>
2023-04-23 20:27:39 +0900

    boj_15652

--- Algorithm/src/Algorithm.java
+++ Algorithm/src/Algorithm.java
@@ -1,35 +1,45 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Algorithm {
 	
+	static int N;
+	static int M;
+	static int[] arr;
+	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		long N = Integer.parseInt(br.readLine());
-		if(N < 10) {
-			System.out.println(N);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[M];
+		
+		dfs(1, 0);
+		
+		System.out.println(sb);
+		
+		
+	}
+	
+	public static void dfs(int at, int depth) {
+		if(depth == M) {
+			for(int i = 0; i < M; i++) {
+				sb.append(arr[i] + " ");
+			}
+			sb.append("\n");
+			return;
 		}
-		else if(N < 100) {
-			System.out.println((N - 9) * 2 + 9);
-		} 
-		else if(N < 1000) {
-			System.out.println((N - 99) * 3 + 90 * 2 + 9);
-		} else if(N < 10000) {
-			System.out.println((N - 999) * 4 + 900 * 3 + 90 * 2 + 9);
-		} else if(N < 100000) {
-			System.out.println((N - 9999) * 5 + 9000 * 4 + 900 * 3 + 90 * 2 + 9);
-		} else if(N < 1000000) {
-			System.out.println((N - 99999) * 6 + 90000 * 5 + 9000 * 4 + 900 * 3 + 90 * 2 + 9);
-		} else if(N < 10000000) {
-			System.out.println((N - 999999) * 7 + 900000 * 6 + 90000 * 5 + 9000 * 4 + 900 * 3 + 90 * 2 + 9);
-		} else if(N < 100000000) {
-			System.out.println((N - 9999999) * 8 + 9000000 * 7 + 900000 * 6 + 90000 * 5 + 9000 * 4 + 900 * 3 + 90 * 2 + 9);
-		} else {
-			System.out.println(90000000 * 8 + 9000000 * 7 + 900000 * 6 + 90000 * 5 + 9000 * 4 + 900 * 3 + 90 * 2 + 9 + 9);
+		
+		for(int i = at; i <= N; i++) {
+			arr[depth] = i;
+			dfs(i, depth + 1);
 		}
 	}
 } 
