47123e2fb7c47a6c5f00ee717be248696659f987
Juyeon Ha <hpt0127@naver.com>
2023-07-25 12:31:33 +0900

    boj_2023

--- Algorithm/src/boj_2023.java
+++ Algorithm/src/boj_2023.java
@@ -5,41 +5,49 @@ import java.io.InputStreamReader;
 public class boj_2023 {
 
 	static int N;
-	static StringBuilder sb = new StringBuilder();
+	static StringBuilder sb;
+	static boolean[] check;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		sb = new StringBuilder();
 		N = Integer.parseInt(br.readLine());
-		DFS(2, 1);
-		DFS(3, 1);
-		DFS(5, 1);
-		DFS(7, 1);
+		dfs(2, 1);
+		dfs(3, 1);
+		dfs(5, 1);
+		dfs(7, 1);
 		
 		System.out.println(sb);
 	}
 	
-	public static void DFS(int num, int digit) {
-		if(digit == N) {
+	public static void dfs(int num, int depth) {
+		if(depth == N) {
 			if(isPrime(num)) {
 				sb.append(num + "\n");
 			}
-			return;
 		}
 		
 		for(int i = 1; i <= 9; i++) {
 			if(i % 2 == 0) continue;
 			if(isPrime(num * 10 + i)) {
-				DFS(num * 10 + i, digit + 1);
+				dfs(num * 10 + i, depth + 1);
 			}
 		}
+		
+		
 	}
 	
 	public static boolean isPrime(int num) {
+		if(num == 1) {
+			return false;
+		}
+		
 		for(int i = 2; i <= Math.sqrt(num); i++) {
-			if(num % i == 0)
+			if(num % i == 0) {
 				return false;
+			}
 		}
 		return true;
 	}
+	
 
 }
