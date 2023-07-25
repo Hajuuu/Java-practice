60a4f812edcfc441f35c0f8c0bce5b656e65688c
Juyeon Ha <hpt0127@naver.com>
2023-07-25 13:45:37 +0900

    boj_13023

--- Algorithm/src/boj_13023.java
+++ Algorithm/src/boj_13023.java
@@ -2,68 +2,64 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class boj_13023 {
 
-	static boolean[] visited;
 	static ArrayList<Integer>[] nodeList;
-	static boolean ans;
+	static boolean[] visit;
+	static boolean answer;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		ans = false;
-		visited = new boolean[N];
 		nodeList = new ArrayList[N];
+		visit = new boolean[N];
+		answer = false;
 		
 		for(int i = 0; i < N; i++) {
 			nodeList[i] = new ArrayList<>();
 		}
-		
+
 		for(int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
+			
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			
 			nodeList[a].add(b);
 			nodeList[b].add(a);
 		}
 		
 		for(int i = 0; i < N; i++) {
-			dfs(i, 1);
-			if(ans) {
+			dfs(1, i);
+			if(answer) {
 				break;
 			}
+			
 		}
-		
-		if(ans) {
-			System.out.println("1");
+		if(answer) {
+			System.out.println(1);
 		} else {
-			System.out.println("0");
+			System.out.println(0);
 		}
-		
-		
 	}
 	
-	public static void dfs(int start, int depth) {
+	public static void dfs(int depth, int start) {
 		if(depth == 5) {
-			ans = true;
+			answer = true;
 			return;
 		}
 		
-		visited[start] = true;
-		
+		visit[start] = true;
 		for(int i : nodeList[start]) {
-			if(!visited[i]) {
-				dfs(i, depth + 1);
+			if(!visit[i]) {
+				dfs(depth + 1, i);
 			}
 		}
-		
-		visited[start] = false;
-		
+		visit[start] = false;
 	}
 	
 
