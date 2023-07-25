dcf251c1a9a3d757a1c8cfe0ba448149090e04a9
Juyeon Ha <hpt0127@naver.com>
2023-07-25 15:15:08 +0900

    boj_1260

--- Algorithm/src/boj_1260.java
+++ Algorithm/src/boj_1260.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -10,79 +9,71 @@ import java.util.StringTokenizer;
 
 public class boj_1260 {
 
-	static int N, M;
 	static ArrayList<Integer>[] nodeList;
-	static boolean[] isVisited;
-	static StringBuilder sb = new StringBuilder();
+	static StringBuilder sb;
+	static boolean[] visit;
+	static int N, M, V;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		int V = Integer.parseInt(st.nextToken());
+		V = Integer.parseInt(st.nextToken());
 		
 		nodeList = new ArrayList[N + 1];
-		isVisited = new boolean[N + 1];
+		visit = new boolean[N + 1];
 		for(int i = 1; i <= N; i++) {
 			nodeList[i] = new ArrayList<>();
 		}
-		
 		for(int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			
 			nodeList[a].add(b);
 			nodeList[b].add(a);
 		}
-		
 		for(int i = 1; i <= N; i++) {
 			Collections.sort(nodeList[i]);
 		}
-		
+		sb = new StringBuilder();
 		dfs(V);
 		sb.append("\n");
-		Arrays.fill(isVisited, false);
 		bfs(V);
 		System.out.println(sb);
-		
 	}
 	
-	public static void dfs(int V) {
-		if(isVisited[V]) return;
+	public static void dfs(int start) {
+		if(visit[start]) {
+			return;
+		}
 		
-		sb.append(V + " ");
-		isVisited[V] = true;
-		
-		for(int i = 0; i < nodeList[V].size(); i++) {
-
-			int cur = nodeList[V].get(i);
-			if(!isVisited[cur]) {
-				dfs(cur);
+		visit[start] = true;
+		sb.append(start + " ");
+		for(int i : nodeList[start]) {
+			if(!visit[i]) {
+				dfs(i);
 			}
 		}
 	}
-	
-	public static void bfs(int V) {
-		Queue<Integer> q = new LinkedList<>();
+
+	public static void bfs(int start) {
+		Queue<Integer> queue = new LinkedList<>();
+		boolean[] check = new boolean[N + 1];
+		queue.offer(start);
+		check[start] = true;
 		
-		q.add(V);
-		isVisited[V] = true;
-		
-		while(!q.isEmpty()) {
-			int node = q.poll();
-			sb.append(node + " ");
-			
-			for(int i = 0; i < nodeList[node].size(); i++) {
-				int cur = nodeList[node].get(i);
-				if(!isVisited[cur]) {
-					q.add(cur);
-					isVisited[cur] = true;
+		while(!queue.isEmpty()) {
+			int next = queue.poll();
+			sb.append(next + " ");
+			for(int i : nodeList[next]) {
+				if(!check[i]) {
+					queue.offer(i);
+					check[i] = true;
 				}
 			}
 		}
 	}
-
+	
+	
 }
