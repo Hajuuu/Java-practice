548d7d1ae680ec807d2bab26cb19fff67756f2ba
Juyeon Ha <hpt0127@naver.com>
2023-07-28 19:48:02 +0900

    boj_2178

--- Algorithm/src/boj_2178.java
+++ Algorithm/src/boj_2178.java
@@ -7,51 +7,54 @@ import java.util.StringTokenizer;
 
 public class boj_2178 {
 
-	static int[] dx = {0, 1, 0, -1};
-	static int[] dy = {1, 0, -1, 0};
+	static int[][] maze;
 	static int[][] arr;
+	static int[] dx = {1, 0, -1, 0};
+	static int[] dy = {0, 1, 0, -1};
 	static int N, M;
-	static boolean[][] visited;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		visited = new boolean[N][M];
-		arr = new int[N][M];
 		
+		arr = new int[N][M];
+		maze = new int[N][M];
+		String str;
 		for(int i = 0; i < N; i++) {
-			String str = br.readLine();
+			str = br.readLine();
 			for(int j = 0; j < M; j++) {
 				arr[i][j] = str.charAt(j) - '0';
 			}
 		}
 		
 		BFS(0, 0);
-		System.out.println(arr[N - 1][M - 1]);
+		
+		System.out.println(maze[N - 1][M - 1]);
+		
 	} 
 	
 	public static void BFS(int x, int y) {
 		Queue<int[]> queue = new LinkedList<>();
 		queue.offer(new int[] {x, y});
-		visited[x][y] = true;
+		maze[x][y] = 1;
+		boolean[][] visit = new boolean[N][M];
 		while(!queue.isEmpty()) {
 			int[] now = queue.poll();
 			for(int i = 0; i < 4; i++) {
-				int a = now[0] + dx[i];
-				int b = now[1] + dy[i];
-				if(a >= 0 && b >= 0 && a < N && b < M) {
-					if(arr[a][b] != 0 && !visited[a][b]) {
-						visited[a][b] = true;
-						arr[a][b] = arr[now[0]][now[1]] + 1;
-						queue.offer(new int[] {a, b});
+				if(now[0] + dx[i] >= 0 && now[0] + dx[i] < N && now[1] + dy[i] >= 0 && now[1] + dy[i] < M) {
+					if(!visit[now[0] + dx[i]][now[1] + dy[i]] && arr[now[0] + dx[i]][now[1] + dy[i]] == 1) {
+						maze[now[0] + dx[i]][now[1] + dy[i]] = maze[now[0]][now[1]] + 1;
+						queue.offer(new int[] {now[0] + dx[i], now[1] + dy[i]});
+						visit[now[0] + dx[i]][now[1] + dy[i]] = true;
 					}
 				}
 			}
+			
 		}
 		
-		
 	}
+	
 
 }
