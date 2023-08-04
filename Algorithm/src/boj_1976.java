301d61b95953c6296fc08fd5d6dcad2e71c20775
Juyeon Ha <hpt0127@naver.com>
2023-08-04 15:46:16 +0900

    boj_1976

--- Algorithm/src/boj_1976.java
+++ Algorithm/src/boj_1976.java
@@ -6,68 +6,66 @@ import java.util.StringTokenizer;
 public class boj_1976 {
 
 	static int[] parent;
-	static int[][] city;
-	static int[] route;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		StringTokenizer st;
+		
 		int N = Integer.parseInt(br.readLine());
 		int M = Integer.parseInt(br.readLine());
-
-		StringTokenizer st;
-
+		
 		parent = new int[N + 1];
-		city = new int[N + 1][N + 1];
-		route = new int[M];
-		
-		for(int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 1; j <= N; j++) {
-				city[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < M; i++) {
-			route[i] = Integer.parseInt(st.nextToken());
-		}
 		
 		for(int i = 1; i <= N; i++) {
 			parent[i] = i;
 		}
+		
 		for(int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
 			for(int j = 1; j <= N; j++) {
-				if(city[i][j] == 1) {
+				if(Integer.parseInt(st.nextToken()) == 1) {
 					union(i, j);
 				}
 			}
 		}
 		
-		int index = find(route[0]);
+		int[] travel = new int[M];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < M; i++) {
+			travel[i] = Integer.parseInt(st.nextToken());
+		}
+
 		for(int i = 1; i < M; i++) {
-			if(index != find(route[i])) {
-				System.out.println("NO\n");
+			if(!find(travel[0], travel[i])) {
+				System.out.println("NO");
 				return;
 			}
 		}
-		
-		System.out.println("YES\n");
-		
+		System.out.println("YES");
+	}
+	
+	public static int getParent(int num) {
+		if(parent[num] == num) return num;
+		return parent[num] = getParent(parent[num]);
 	}
 	
 	public static void union(int a, int b) {
-		a = find(a);
-		b = find(b);
-		
-		if(a != b) {
+		a = getParent(a);
+		b = getParent(b);
+		if(a < b) {
 			parent[b] = a;
+		} else {
+			parent[a] = b;
 		}
 	}
-
-	public static int find(int a) {
-		if(a == parent[a]) {
-			return a;
+	
+	public static boolean find(int a, int b) {
+		a = getParent(a);
+		b = getParent(b);
+		
+		if(a == b) {
+			return true;
+		} else {
+			return false;
 		}
-		return parent[a] = find(parent[a]);
 	}
 }
