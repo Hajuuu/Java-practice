2c14506acd5c69de47d4d6210959ba72e93f1acb
Juyeon Ha <hpt0127@naver.com>
2023-08-17 10:34:38 +0900

    boj_24445

--- Algorithm/src/boj_24445.java
+++ Algorithm/src/boj_24445.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -39,7 +39,7 @@ public class boj_24445 {
 		}
 		
 		for(int i = 1; i <= N; i++) {
-			nodeList[i].sort(Collections.reverseOrder());
+			nodeList[i].sort(Comparator.reverseOrder());
 		}
 		BFS(R);
 		
