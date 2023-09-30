3e23a4faf064937c68534287b5789d071b032fc2
Juyeon Ha <hpt0127@naver.com>
2023-09-30 23:29:25 +0900

    boj_2193

--- Algorithm/src/boj_2193.java
+++ Algorithm/src/boj_2193.java
@@ -13,14 +13,12 @@ public class boj_2193 {
 	}
 
 	public static long pinary(int N) {
-		long[][] dp = new long[N + 1][2];
-		dp[1][0] = 0;
-		dp[1][1] = 1;
+		long[] dp = new long[N + 1];
+		dp[1] = 1;
 		for(int i = 2; i <= N; i++) {
-			dp[i][0] = dp[i - 1][1] + dp[i - 1][0];
-			dp[i][1] = dp[i - 1][0];
+			dp[i] = dp[i - 2] + dp[i - 1];
 		}
 		
-		return dp[N][0] + dp[N][1];
+		return dp[N];
 	}
 }
