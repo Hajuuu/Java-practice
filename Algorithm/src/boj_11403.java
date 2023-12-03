2eccab37a7a04abd74afe8ed51b00e78ebed58b0
Hajuuu <hpt0127@naver.com>
2023-12-03 18:35:30 +0900

    boj_11403_floyd

--- Algorithm/src/boj_11403.java
+++ Algorithm/src/boj_11403.java
@@ -1,66 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class boj_11403 {
 
-	static ArrayList<Integer>[] nodeList;
-	static boolean[] visit;
+	static int[][] nodeList;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		nodeList = new ArrayList[N];
+		nodeList = new int[N][N];
 		
 		StringTokenizer st;
 		for(int i = 0; i < N; i++) {
-			nodeList[i] = new ArrayList<>();
-		}
-		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < N; j++) {
-				if(Integer.parseInt(st.nextToken()) == 1) {
-					nodeList[i].add(j);
-				}
+				nodeList[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
+		for(int k = 0; k < N; k++) {
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					if(nodeList[i][k] == 1 && nodeList[k][j] == 1) {
+						nodeList[i][j] = 1;
+					}
+				}
+			}
+		}
 		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < N; j++) {
-				visit = new boolean[N];
-				boolean check = bfs(i, j);
-				if(check) {
-					sb.append("1 ");
-				}
-				else {
-					sb.append("0 ");
-				}
+				sb.append(nodeList[i][j] + " ");
 			}
 			sb.append("\n");
 		}
 		System.out.println(sb);
-		
-	}
-	
-	public static boolean bfs(int a, int b) {
-		Queue<Integer> queue = new ArrayDeque<>();
-		queue.offer(a);
-		while(!queue.isEmpty()) {
-			int now = queue.poll();
-			for(int i : nodeList[now]) {
-				if(visit[i]) continue;
-				if(i == b) {
-					return true;
-				}
-				queue.offer(i);
-				visit[i] = true;
-			}
-		}
-		return false;
 	}
 
 }
