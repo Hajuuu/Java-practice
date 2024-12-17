8e2ec366283f12729ce14b527421ac94b4449d1a
Hajuuu <96831825+Hajuuu@users.noreply.github.com>
2024-12-17 22:37:59 +0900

    프로그래머스 Level01 삼총사

--- Algorithm/src/programmers_20.java
+++ Algorithm/src/programmers_20.java
@@ -1,1 +1,23 @@
-.
+class Solution {
+    static int answer = 0;
+    static int[] num;
+    public int solution(int[] number) {
+        num = number;
+        dfs(0, 0, 0);
+        return answer;
+    }
+    
+    public static void dfs(int start, int count, int sum) {
+        
+        if(count == 3) {
+            if(sum == 0) {
+                answer++;
+                return;
+            }
+        }
+        
+        for(int i = start; i < num.length; i++) {
+            dfs(i + 1, count + 1, sum + num[i]);
+        }
+    }
+}
