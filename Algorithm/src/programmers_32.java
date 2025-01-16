0dc897e20436c793b5a61a742c019bca92dc2237
Hajuuu <96831825+Hajuuu@users.noreply.github.com>
2025-01-16 23:18:52 +0900

    프로그래머스 Level01 카드 뭉치

---
+++ Algorithm/src/programmers_32.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        int a = 0;
+        int b = 0;
+        for(int i = 0; i < goal.length; i++) {
+            if(a < cards1.length && cards1[a].equals(goal[i])) {
+                a++;
+            } else if(b < cards2.length && cards2[b].equals(goal[i])) {
+                b++;
+            } else {
+                return "No";
+            }
+        }
+        return "Yes";
+        
+    }
+}
