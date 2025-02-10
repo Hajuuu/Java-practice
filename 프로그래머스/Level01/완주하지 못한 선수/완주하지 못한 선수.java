cc137b3ba5b825c7ac598b94ef34a6d8c82eb78d
Hajuuu <96831825+Hajuuu@users.noreply.github.com>
2025-02-10 23:50:12 +0900

    Create 완주하지 못한 선수.java

---
+++ 프로그래머스/Level01/완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> hm = new HashMap<>();
+        for(String p : participant) {
+            hm.put(p, hm.getOrDefault(p, 0) + 1);
+        }
+        for(String c : completion) {
+            hm.put(c, hm.get(c) - 1);
+        }
+        for(String p : hm.keySet()) {
+            if(hm.get(p) != 0) {
+                return p;
+            }
+        }
+        return "";
+    }
+}
