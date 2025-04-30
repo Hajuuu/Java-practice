30fb0192eba1e65a079774146e1d6548f0e11c23
Hajuuu <96831825+Hajuuu@users.noreply.github.com>
2025-04-30 14:45:48 +0900

    백준 2579 다른 풀이

--- Algorithm/src/boj_2579.java
+++ Algorithm/src/boj_2579.java
@@ -2,35 +2,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class boj_2579 {
+public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		
-		int[] arr = new int[N + 1];
-		for(int i = 1; i <= N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		int answer = stairs(arr, N);
-		System.out.println(answer);
-	}
-	
-	public static int stairs(int[] arr, int N) {
-		int[][] score = new int[N + 1][2];
-		if(N < 2) {
-			return arr[1];
-		}
-		score[1][0] = arr[1];
-		score[1][1] = 0;
-		score[2][0] = arr[1] + arr[2];
-		score[2][1] = arr[2];
-		for(int i = 3; i <= N; i++) {
-			score[i][0] = score[i - 1][1] + arr[i];
-			score[i][1] = Math.max(score[i - 2][0] + arr[i], score[i - 2][1] + arr[i]);
-		} 
-		return Math.max(score[N][0], score[N][1]);
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] scores = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            scores[i] = Integer.parseInt(br.readLine());
+        }
+        if (N < 2) {
+            System.out.println(scores[1]);
+            return;
+        }
+        int[] dp = new int[N + 1];
+        dp[1] = scores[1];
+        dp[2] = scores[1] + scores[2];
+        for (int i = 3; i <= N; i++) {
+            dp[i] = Math.max(dp[i - 3] + scores[i - 1], dp[i - 2]) + scores[i];
+        }
+
+        System.out.println(dp[N]);
+    }
 
 }
