3b1e880975316871074da9a64855e773f79937c5
Hajuuu <96831825+Hajuuu@users.noreply.github.com>
2025-05-03 23:52:53 +0900

    백준 1753 다른 풀이

--- Algorithm/src/boj_1753.java
+++ Algorithm/src/boj_1753.java
@@ -1,91 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class boj_1753 {
+public class Main {
 
-	static ArrayList<mNode>[] nodeList;
-	static boolean[] visit;
-	static int[] distance;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int V = Integer.parseInt(st.nextToken());
-		int E = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(br.readLine());
-		
-		nodeList = new ArrayList[V + 1];
-		visit = new boolean[V + 1];
-		distance = new int[V + 1];
-		
-		for(int i = 1; i <= V; i++) {
-			nodeList[i] = new ArrayList<>();
-		}
-	
-		Arrays.fill(distance, Integer.MAX_VALUE);
-		for(int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			
-			nodeList[u].add(new mNode(v, w));
-			
-		}
-		dijkstra(K);
-		StringBuilder sb = new StringBuilder();
-		for(int i = 1; i <= V; i++) {
-			if(distance[i] == Integer.MAX_VALUE) {
-				sb.append("INF\n");
-			} else {
-				sb.append(distance[i] + "\n");
-			}
-		} 
-		
-		System.out.println(sb);
-	}
-	
-	public static void dijkstra(int start) {
-		PriorityQueue<mNode> queue = new PriorityQueue<>();
-		distance[start] = 0;
-		queue.offer(new mNode(start, 0));
-		
-		while(!queue.isEmpty()) {
-			mNode n = queue.poll();
-			int v = n.idx;
-			if(visit[v]) continue;
-			visit[v] = true;
-			
-			for(mNode i : nodeList[v]) {
-				int next = i.idx;
-				int weight = i.cost;
-				if(distance[next] > distance[v] + weight) {
-					distance[next] = distance[v]  + weight;
-					queue.offer(new mNode(next, distance[next]));
-				}
-			}
-		}
-	}
+    static List<List<Node>> nodeList;
+    static int[] distance;
 
-	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(br.readLine());
+
+        nodeList = new ArrayList<>();
+        distance = new int[V + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        for (int i = 0; i <= V; i++) {
+            nodeList.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            nodeList.get(u).add(new Node(v, w));
+        }
+
+        dijkstra(K);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= V; i++) {
+            if (distance[i] == Integer.MAX_VALUE) {
+                sb.append("INF\n");
+                continue;
+            }
+            sb.append(distance[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static void dijkstra(int start) {
+        PriorityQueue<Node> queue = new PriorityQueue<>(Comparator.comparingInt(o -> o.value));
+        queue.offer(new Node(start, 0));
+        distance[start] = 0;
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            if (distance[node.num] < node.value) {
+                continue;
+            }
+            for (Node n : nodeList.get(node.num)) {
+                if (distance[n.num] > distance[node.num] + n.value) {
+                    distance[n.num] = distance[node.num] + n.value;
+                    queue.offer(new Node(n.num, distance[n.num]));
+                }
+
+            }
+        }
+    }
 }
 
-class mNode implements Comparable<mNode> {
-	int idx;
-	int cost;
-	
-	public mNode(int idx, int cost) {
-		this.idx = idx;
-		this.cost = cost;
-	}
-	
-	@Override
-	public int compareTo(mNode o) {
-		return this.cost - o.cost;
-	}
+class Node {
+    int num;
+    int value;
+
+    Node(int num, int value) {
+        this.num = num;
+        this.value = value;
+    }
 }
