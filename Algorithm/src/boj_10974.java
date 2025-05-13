ad6cfd66bfafff36770c10ba7c550f9fe4c2d57c
Hajuuu <96831825+Hajuuu@users.noreply.github.com>
2025-05-13 15:35:06 +0900

    백준 10974 약간 다른 풀이

--- Algorithm/src/boj_10974.java
+++ Algorithm/src/boj_10974.java
@@ -1,42 +1,41 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class boj_10974 {
+public class Main {
 
-	static boolean[] visit;
-	static int[] arr;
-	static int N;
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		N = Integer.parseInt(br.readLine());
-		
-		visit = new boolean[N + 1];
-		arr = new int[N];
-		
-		printNum(0);
-		System.out.println(sb);
-	}
-	
-	public static void printNum(int depth) {
-		if(depth == N) {
-			for(int i : arr) {
-				sb.append(i + " ");
-			}
-			sb.append("\n");
-			return;
-		}
-		
-		for(int i = 1; i <= N; i++) {
-			if(!visit[i]) {
-				arr[depth] = i;
-				visit[i] = true;
-				printNum(depth + 1);
-				visit[i] = false;
-			}
-		}
-	}
+    static int N;
+    static int[] arr;
+    static StringBuilder sb;
+    static boolean[] visit;
 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        sb = new StringBuilder();
+        visit = new boolean[N + 1];
+        backtracking(0);
+        System.out.println(sb);
+
+    }
+
+    public static void backtracking(int depth) {
+        if (depth == N) {
+            for (int i : arr) {
+                sb.append(i + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (visit[i]) {
+                continue;
+            }
+            visit[i] = true;
+            arr[depth] = i;
+            backtracking(depth + 1);
+            visit[i] = false;
+        }
+    }
 }
