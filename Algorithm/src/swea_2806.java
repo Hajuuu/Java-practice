3b1930b3d6209ae5e0c6f6b8632316d60eda55f8
Hajuuu <96831825+Hajuuu@users.noreply.github.com>
2025-05-22 15:27:58 +0900

    SWEA 2806 N-Queen 다른 풀이

--- Algorithm/src/swea_2806.java
+++ Algorithm/src/swea_2806.java
@@ -1,46 +1,53 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class swea_2806 {
-	
-	static int[] board;
-	static int N;
-	static int count;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		for(int tc = 1; tc <= T; tc++) {
-			N = Integer.parseInt(br.readLine());
-			count = 0;
-			board = new int[N];
-			dfs(0);
-			sb.append("#" + tc + " " + count + "\n");
-		}
-		System.out.println(sb);
-	}
-	
-	public static void dfs(int depth) {
-		if(depth == N) {
-			count++;
-			return;
-		}
-		
-		for(int i = 0; i < N; i++) {
-			board[depth] = i;
-			if(check(depth)) {
-				dfs(depth + 1);
-			}
-		}
-	}
-	
-	public static boolean check(int num) {
-		for(int i = 0; i < num; i++) {
-			if(board[i] == board[num] || ((board[num] - board[i] == num - i) || (board[i] - board[num] == num - i))) {
-				return false;
-			}
-		}
-		return true;
-	}
+public class Solution {
+
+    /**
+     * board[i] = j;
+     * i : row, j : col
+     * 퀸을 놓을 수 있는 곳 : 대각선, 위, 아래로 없어야함
+     * nQueen 함수에서 row 하나씩 증가시키면서 만약 row가 N이랑 같으면 count 증가
+     */
+    static int[] board;
+    static int N;
+    static int count;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int tc = 1; tc <= T; tc++) {
+            N = Integer.parseInt(br.readLine());
+            board = new int[N];
+            count = 0;
+            nQueen(0, 0);
+            sb.append("#" + tc + " " + count + "\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static void nQueen(int row, int col) {
+        if (row == N) {
+            count++;
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (check(row, i)) {
+                board[row] = i;
+                nQueen(row + 1, i);
+            }
+        }
+    }
+
+    public static boolean check(int row, int col) {
+        for (int i = 0; i < row; i++) {
+            if (board[i] == col || (row - i) == Math.abs(board[i] - col)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
