1021ebf646ea7625dc266d57cf754508c031de36
Hajuuu <96831825+Hajuuu@users.noreply.github.com>
2025-06-04 10:38:22 +0900

    백준 1759 다른 풀이

--- Algorithm/src/boj_1759.java
+++ Algorithm/src/boj_1759.java
@@ -4,67 +4,60 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class boj_1759 {
+public class Main {
 
-	static char[] list;
-	static char[] dlist;
-	static boolean[] visited;
-	static int L, C;
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		
-		L = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		
-		list = new char[C];
-		dlist = new char[L];
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < C; i++) {
-			list[i] = st.nextToken().charAt(0);
-		}
-		
-		Arrays.sort(list);
-		DFS(0, 0);
-		System.out.println(sb);
-	}
-	
-	public static void DFS(int at, int depth) {
-		if(depth == L) {
-			if(isValid()) {
-				for(int i = 0; i < L; i++) {
-					sb.append(dlist[i]);
-				}
-				sb.append("\n");
-			}
-			return;
-		}
-		
-		for(int i = at; i < C; i++) {
-			dlist[depth] = list[i];
-			DFS(i + 1, depth + 1);
-			
-		}
-	}
-	
-	public static boolean isValid() {
-		int count1 = 0;
-		int count2 = 0;
-		
-		for(char i : dlist) {
-			if(i == 'a' || i == 'e' || i == 'i' || i == 'o' ||i == 'u') {
-				count1++;
-			} else {
-				count2++;
-			}
-		}
-		
-		if(count1 >= 1 && count2 >= 2) {
-			return true;
-		}
-		return false;
-	}
+    static int L, C;
+    static boolean[] visited;
+    static StringBuilder sb;
+    static String[] letters;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        letters = new String[C];
+        for (int i = 0; i < C; i++) {
+            letters[i] = st.nextToken();
+        }
+        sb = new StringBuilder();
+        visited = new boolean[C];
+        Arrays.sort(letters);
+        dfs("", 0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(String str, int idx) {
+        if (str.length() == L) {
+            if (check(str)) {
+                sb.append(str + "\n");
+            }
+            return;
+        }
+
+        for (int i = idx; i < letters.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs(str + letters[i], i + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static boolean check(String str) {
+        int v = 0;
+        int c = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+                v++;
+            } else {
+                c++;
+            }
+        }
+        if (v > 0 && c > 1) {
+            return true;
+        }
+        return false;
+    }
 }
