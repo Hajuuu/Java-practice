432dcee6a8a0370ad2023ae4b25fcf25cea8613b
Hajuuu <96831825+Hajuuu@users.noreply.github.com>
2025-06-09 18:55:43 +0900

    백준 11725 다른 풀이

--- Algorithm/src/boj_11725.java
+++ Algorithm/src/boj_11725.java
@@ -1,55 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-	static int N;
-	static ArrayList<Integer>[] nodeList;
-	static boolean[] visited;
-	static int[] answer;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		N = Integer.parseInt(br.readLine());
-		
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		nodeList = new ArrayList[N + 1];
-		visited = new boolean[N + 1];
-		answer = new int[N + 1];
-		for(int i = 1; i <= N; i++) {
-			nodeList[i] = new ArrayList<>();
-		}
-		
-		for(int i = 0; i < N - 1; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			nodeList[a].add(b);
-			nodeList[b].add(a);
-		}
-		
-		DFS(1);
-		for(int i = 2; i <= N; i++) {
-			sb.append(answer[i] + "\n");
-		}
-		System.out.println(sb);
-	}
-	
-	public static void DFS(int start) {
-		visited[start] = true;
-		
-		for(int i : nodeList[start]) {
-			if(!visited[i]) {
-				answer[i] = start;
-				visited[i] = true;
-				DFS(i);
-			}
-		}
-	}
+    static List<List<Integer>> tree;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        tree = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            tree.add(new ArrayList<>());
+        }
+        for (int i = 0; i < N - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            tree.get(a).add(b);
+            tree.get(b).add(a);
+        }
+
+        int[] index = new int[N + 1];
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.offer(1);
+        index[1] = -1;
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (int i : tree.get(cur)) {
+                if (index[i] == 0) {
+                    index[i] = cur;
+                    queue.offer(i);
+                }
+
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i <= N; i++) {
+            sb.append(index[i] + "\n");
+        }
+        System.out.println(sb);
+    }
 }
