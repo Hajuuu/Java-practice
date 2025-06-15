ffe8f9c1b1532c222b5ba587e1b5a966b71b8190
Hajuuu <96831825+Hajuuu@users.noreply.github.com>
2025-06-15 15:17:12 +0900

    백준 11724 다른 풀이

--- Algorithm/src/boj_11724.java
+++ Algorithm/src/boj_11724.java
@@ -2,70 +2,52 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.List;
 import java.util.StringTokenizer;
 
-public class boj_11724 {
+public class Main {
 
+    static List<List<Integer>> graph;
+    static boolean[] visited;
 
-	static ArrayList<Integer>[] nodeList;
-	static int N, M;
-	static boolean[] visit;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        graph = new ArrayList<>();
+        visited = new boolean[N + 1];
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
 
-		nodeList = new ArrayList[N + 1];
-		visit = new boolean[N + 1];
-		for(int i = 1; i <= N; i++) {
-			nodeList[i] = new ArrayList<>();
-		}
-		for(int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			
-			nodeList[u].add(v);
-			nodeList[v].add(u);
-		}
-		int count = 0;
-		for(int i = 1; i <= N; i++) {
-			if(!visit[i]) {
-				bfs(i);
-				count++;
-			}
-		}
-		
-		System.out.println(count);
-	}
-	
-//	public static void dfs(int u) {
-//		if(visit[u]) return;
-//		visit[u] = true;
-//		
-//		for(int i : nodeList[u]) {
-//			if(!visit[i]) {
-//				dfs(i);
-//			}
-//		}
-//	}
-	
-	public static void bfs(int u) {
-		Queue<Integer> queue = new LinkedList<>();
-		queue.offer(u);
-		
-		while(!queue.isEmpty()) {
-			int v = queue.poll();
-			for(int i : nodeList[v]) {
-				if(!visit[i]) {
-					queue.offer(i);
-					visit[i] = true;
-				}
-			}
-		}
-	}
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        int answer = 0;
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    public static void dfs(int u) {
+        for (int i : graph.get(u)) {
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs(i);
+            }
+
+        }
+    }
+
 }
+
