ec6da25a240759e2ad37424ea0479f9537e3ff6c
Hajuuu <96831825+Hajuuu@users.noreply.github.com>
2025-11-13 21:00:47 +0900

    Update boj_9466.java

--- Algorithm/src/boj_9466/boj_9466.java
+++ Algorithm/src/boj_9466/boj_9466.java
@@ -1,81 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static ArrayList<Integer>[] nodeList;
-	static boolean[] visit;
-	static int N; 
-	static Set<Integer> set;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		for(int tc = 0; tc < T; tc++) {
-			N = Integer.parseInt(br.readLine());
-			nodeList = new ArrayList[N + 1];
-			visit = new boolean[N + 1];
-			st = new StringTokenizer(br.readLine());
-			for(int i = 1; i <= N; i++) {
-				nodeList[i] = new ArrayList<>();
-			}
-			int answer = 0;
-			for(int i = 0; i < N; i++) {
-				int x = Integer.parseInt(st.nextToken());
-				nodeList[i + 1].add(x);
-				if(x == (i + 1)) {
-					visit[x] = true;
-					answer++;
-				}
-			}
-			set = new HashSet<>();
-			
-			for(int i = 1; i <= N; i++) {
-				set = new HashSet<>();
-				if(!visit[i]) {
-					boolean check = dfs(i);
-					if(check) {
-						answer += set.size();
-					}
-				}
-				
-			}
-			
-			sb.append(N - answer + "\n");
-		}
-		System.out.println(sb);
-	}
-	
-	public static boolean dfs(int num) {
-		Stack<Integer> stack = new Stack<>();
-		Stack<Integer> check = new Stack<>();
-		stack.push(num);
-		check.push(num);
-		set.add(num);
-		visit[num] = true;
-		while(!stack.isEmpty()) {
-			int now = stack.pop();
-			for(int i : nodeList[now]) {
-				if(check.contains(i)) {
-					visit[i] = true;
-					return true;
-				}
-				if(!visit[i]) {
-					visit[i] = true;
-					set.add(i);
-					stack.push(i);
-					check.push(i);
-				}
-			}
-		}
-		return false;
-	}
+
+    static int[] nodes;
+    static boolean[] visited;
+    static boolean[] done;
+    static int count;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 0; tc < T; tc++) {
+            int n = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            nodes = new int[n + 1];
+            for (int i = 1; i <= n; i++) {
+                nodes[i] = Integer.parseInt(st.nextToken());
+            }
+            visited = new boolean[n + 1];
+            done = new boolean[n + 1];
+            count = 0;
+            for (int i = 1; i <= n; i++) {
+                if (done[i]) {
+                    continue;
+                }
+                dfs(i);
+            }
+            sb.append(n - count + "\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static void dfs(int start) {
+        if (done[start]) {
+            return;
+        }
+        if (visited[start]) {
+            done[start] = true;
+            count++;
+        }
+        visited[start] = true;
+        dfs(nodes[start]);
+        visited[start] = false;
+        done[start] = true;
+    }
 
 }
