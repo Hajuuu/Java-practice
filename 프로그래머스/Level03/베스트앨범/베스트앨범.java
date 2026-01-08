ebfcf2e51b99b8b59a35f178ab9b0e90b95655c3
Hajuuu <96831825+Hajuuu@users.noreply.github.com>
2026-01-09 02:12:28 +0900

    Update 베스트앨범.java

--- 프로그래머스/Level03/베스트앨범/베스트앨범.java
+++ 프로그래머스/Level03/베스트앨범/베스트앨범.java
@@ -1,51 +1,47 @@
 import java.util.*;
-
 class Solution {
-    
     public int[] solution(String[] genres, int[] plays) {
-        Map<String, Integer> genrePlayCount = new HashMap<>();
-        Map<String, List<Song>> genreToSongs = new HashMap<>();
-
-        for (int i = 0; i < genres.length; i++) {
-            String genre = genres[i];
-            int play = plays[i];
-
-            genrePlayCount.put(genre, genrePlayCount.getOrDefault(genre, 0) + play);
-
-            if (!genreToSongs.containsKey(genre)) {
-                genreToSongs.put(genre, new ArrayList<>());
-            }
-            genreToSongs.get(genre).add(new Song(i, play));
-
+        Map<String, Integer> genreCount = new HashMap<>();
+        for(int i = 0; i < genres.length; i++) {
+            genreCount.put(genres[i], genreCount.getOrDefault(genres[i], 0) + plays[i]);
         }
-
-        List<String> sortedGenres = new ArrayList<>(genrePlayCount.keySet());
-        sortedGenres.sort((a, b) -> genrePlayCount.get(b) - genrePlayCount.get(a));
-
-        List<Integer> result = new ArrayList<>();
-        for (String genre : sortedGenres) {
-            List<Song> songs = genreToSongs.get(genre);
-            songs.sort((a, b) -> {
-                if (a.play == b.play) {
-                    return a.index - b.index;
+        List<String> genreKey = new ArrayList<>(genreCount.keySet());
+        genreKey.sort((o1, o2) -> genreCount.get(o2).compareTo(genreCount.get(o1)));
+        
+        Map<String, List<int[]>> playCount = new HashMap<>();
+        for(int i = 0; i < genres.length; i++) {
+            List<int[]> song = playCount.getOrDefault(genres[i], new ArrayList<>());
+            song.add(new int[] {i, plays[i]});
+            playCount.put(genres[i], song);
+        }
+        
+        List<String> playKey = new ArrayList<>(playCount.keySet());
+        for(String play : playKey) {
+            List<int[]> song = playCount.get(play);
+            Collections.sort(song, (o1, o2) -> {
+                if(o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
                 }
-                return b.play - a.play;
+                return o2[1] - o1[1];
             });
-
-            for (int i = 0; i < Math.min(2, songs.size()); i++) {
-                result.add(songs.get(i).index);
-            }
+            playCount.put(play, song);
         }
-
-        return result.stream().mapToInt(i -> i).toArray();
-    }
-}
-class Song {
-    int index;
-    int play;
-
-    Song(int index, int play) {
-        this.index = index;
-        this.play = play;
+        
+        List<Integer> answer = new ArrayList<>();
+        for(String genre : genreKey) {
+            List<int[]> song = playCount.get(genre);
+            if(song.size() < 2) {
+                answer.add(song.get(0)[0]);
+                continue;
+            }
+            answer.add(song.get(0)[0]);
+            answer.add(song.get(1)[0]);
+        }
+        int[] nums = new int[answer.size()];
+        for(int i = 0; i < nums.length; i++) {
+            nums[i] = answer.get(i);
+        }
+        
+        return nums;
     }
 }
