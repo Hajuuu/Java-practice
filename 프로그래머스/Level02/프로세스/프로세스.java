36f1e8ae23c917a2cc99e886ba994ab7e67bd9e0
Hajuuu <96831825+Hajuuu@users.noreply.github.com>
2026-01-15 14:22:19 +0900

    Update 프로세스.java

--- 프로그래머스/Level02/프로세스/프로세스.java
+++ 프로그래머스/Level02/프로세스/프로세스.java
@@ -1,23 +1,30 @@
 import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
+        PriorityQueue<Integer> priority = new PriorityQueue<>(Collections.reverseOrder());
+        Queue<int[]> queue = new ArrayDeque<>();
         int answer = 0;
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
-        for(int i : priorities) {
-            queue.offer(i);
+        
+        
+        for(int i = 0; i < priorities.length; i++) {
+            priority.offer(priorities[i]);
+            queue.offer(new int[] {i, priorities[i]});
         }
         
         while(!queue.isEmpty()) {
-            for(int i = 0; i < priorities.length; i++) {
-                if(priorities[i] == queue.peek()) {
-                    queue.poll();
-                    answer++;
-                    if(i == location) {
-                        return answer;
-                    }
+            int[] now = queue.peek();
+            if(priority.peek() == now[1]) {
+                answer++;
+                if(location == now[0]) {
+                    return answer;
                 }
+                queue.poll();
+                priority.poll();
+                continue;
             }
+            queue.offer(queue.poll());
         }
+        
         return answer;
     }
 }
