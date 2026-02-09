27f66243f9d1ad8ce694ded8cf3c5e09b8c6c871
Hajuuu <96831825+Hajuuu@users.noreply.github.com>
2026-02-09 15:23:00 +0900

    Update 큰 수 만들기.java

--- 프로그래머스/Level02/큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/Level02/큰 수 만들기/큰 수 만들기.java
@@ -1,18 +1,26 @@
+import java.util.*;
 class Solution {
     public String solution(String number, int k) {
-        int len = number.length() - k;
-        int start = 0;
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < len; i++) {
-            int max = 0;
-            for(int j = start; j <= i + k; j++) {
-                if(number.charAt(j) - '0' > max) {
-                    start = j + 1;
-                    max = number.charAt(j) - '0';
-                }
+        Stack<Integer> stack = new Stack<>();
+        int count = 0;
+        for(int i = 0; i < number.length(); i++) {
+            int now = number.charAt(i) - '0';
+            while(!stack.isEmpty() && stack.peek() < now && count < k) {
+                stack.pop();
+                count++;
             }
-            sb.append(max);
+            stack.push(now);
         }
+        
+        for(int i = 0; i < k - count; i++) {
+            stack.pop();
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(int num : stack) {
+            sb.append(num);
+        }
+        
         return sb.toString();
     }
 }
